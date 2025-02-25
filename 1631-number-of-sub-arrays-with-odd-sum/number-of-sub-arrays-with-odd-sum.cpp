ff5781bdfa550dff77b849fbebffbf847f6d814b
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-02-25 09:53:15 +0530

    Time: 3 ms (73.02%) | Memory: 112.1 MB (55.17%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        unordered_map<int, int> m;
+        int zero = 0, one = 0;
         int sum = 0, ans = 0;
         int n = arr.size();
-        m[0] = 1;
+        zero = 1;
         for(int i = 0; i < n; i++) {
             sum += arr[i];
             if(sum % 2 == 0) {
-                if(m.find(1) != m.end()) ans += m[1];
+                ans += one;
+                zero++;
             }
             else {
-                if(m.find(0) != m.end()) ans += m[0];
+                ans += zero;
+                one++;
             }
             ans %= 1000000007;
-            m[sum % 2]++;
         }
         return ans;
     }
