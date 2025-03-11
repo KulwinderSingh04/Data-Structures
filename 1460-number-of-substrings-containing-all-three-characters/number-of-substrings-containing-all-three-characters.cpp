c97f646cdc7dcd5649cbc841d5d895d10ca97ab0
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-03-11 11:25:37 +0530

    Time: 5 ms (76.93%) | Memory: 10.9 MB (81.05%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -4,14 +4,13 @@ public:
         int i = 0;
         int j = 0;
         int ans = 0;
-        unordered_map<char, int> m;
+        vector<int> m(3);
         int n = s.size();
         while(j < n) {
-            m[s[j]]++;
-            while(m.size() == 3) {
-                ans += n - j ;
-                m[s[i]]--;
-                if(m[s[i]] == 0) m.erase(s[i]);
+            m[s[j] - 97]++;
+            while(m[0] > 0 && m[1] > 0 && m[2] > 0) {
+                ans += n - j;
+                m[s[i] - 97]--;
                 i++;
             }
             j++;
