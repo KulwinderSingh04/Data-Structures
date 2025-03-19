5a9a297d2702680522db6da6573c9f3481bb7113
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-03-19 10:32:32 +0530

    Time: 1 ms (86.31%) | Memory: 110.1 MB (91.41%) - LeetSync

--- 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.cpp
@@ -4,16 +4,15 @@ public:
         int flipCount = 0;
         int n = nums.size();
         int count = 0;
-        vector<bool> flip(n, false);
         for(int i = 0; i < n; i++) {
             if(i - k >= 0) {
-                if(flip[i- k]) flipCount--;
+                if(nums[i- k] == 2) flipCount--;
             }
             if((flipCount + nums[i]) % 2 == 0) {
                 if(i + k > n) return -1;
                 flipCount++;
                 count++;
-                flip[i] = true;
+                nums[i] = 2;
             }
         }
         return count;
