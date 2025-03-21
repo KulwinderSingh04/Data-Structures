890ab9304a29194199dfcdef0167dc5581d41d0a
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-03-21 17:31:33 +0530

    Time: 4 ms (71.72%) | Memory: 19.3 MB (67.72%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-
         vector<vector<int>> adj(n);
         vector<int> indegree(n);
         for(int i = 0; i < prerequisites.size(); i++) {
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-            indegree[prerequisites[i][1]]++;
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
         }
         queue<int> q;
         for(int i = 0; i < n; i++) {
