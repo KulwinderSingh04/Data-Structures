73b3bf5419995c8b42162a443b3854d292536f3c
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-03-27 09:28:21 +0530

    Time: 12 ms (65.57%) | Memory: 94 MB (63.52%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> v = nums;
+        nth_element(v.begin(), v.begin() + n / 2, v.end());
+        int count = 0;
+        int idx = -1;
+        int one = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == v[n / 2]) count++;
+            if(count > (i + 1) / 2 && idx == -1) {
+                idx = i;
+                one = count;
+            }
+        }
+        if(count - one >= (n - 1 - idx) / 2 + 1) return idx;
+        return -1;
+    }
+};
