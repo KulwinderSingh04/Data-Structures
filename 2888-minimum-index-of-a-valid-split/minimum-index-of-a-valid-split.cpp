267cbcd0ffbf639646b22d3d228d423de1dfb3ab
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-03-27 09:29:33 +0530

    Time: 8 ms (67.21%) | Memory: 94 MB (63.11%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -6,15 +6,14 @@ public:
         nth_element(v.begin(), v.begin() + n / 2, v.end());
         int count = 0;
         int idx = -1;
-        int one = 0;
         for(int i = 0; i < n; i++) {
             if(nums[i] == v[n / 2]) count++;
             if(count > (i + 1) / 2 && idx == -1) {
                 idx = i;
-                one = count;
+                // one = count;
             }
         }
-        if(count - one >= (n - 1 - idx) / 2 + 1) return idx;
+        if(count - ((idx + 1) / 2 + 1) >= (n - 1 - idx) / 2 + 1) return idx;
         return -1;
     }
 };
