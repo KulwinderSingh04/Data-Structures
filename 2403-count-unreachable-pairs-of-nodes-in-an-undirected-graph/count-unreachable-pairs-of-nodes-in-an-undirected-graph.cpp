b57649dbfffdc25cb303a3ff2774236643008f3c
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-03-28 11:11:52 +0530

    Time: 50 ms (72.72%) | Memory: 151 MB (77.05%) - LeetSync

---
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int find(int x, vector<int>& par) {
+        if(x == par[x]) return x;
+        return par[x] = find(par[x], par);
+    }
+    
+    void Union(int x, int y, vector<int>& par, vector<int>& rank) {
+        int x_par = find(x, par);
+        int y_par = find(y, par);
+        if(x_par == y_par) return;
+        if(rank[x_par] == rank[y_par]) {
+            par[x_par] = par[y_par];
+            rank[y_par]++;
+        }
+        else if(rank[x_par] > rank[y_par]) {
+            par[y_par] = par[x_par];
+        }
+        else {
+            par[x_par] = par[y_par];
+        }
+    }
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        vector<int> par(n);
+        vector<int> rank(n);
+        for(int i = 0; i < n; i++) par[i] = i;
+        for(int i = 0; i < edges.size(); i++) {
+            if(find(edges[i][0], par) != find(edges[i][1], par)) {
+                Union(edges[i][0], edges[i][1], par, rank);
+            }
+        }
+        unordered_map<int, int> m;
+        long long res = 0;
+        for(int i = 0; i < n; i++) {
+            int p = find(i, par);
+            m[p]++;
+        }
+        int rem = n;
+        long long ans= 0;
+        for(auto x : m) {
+            ans += 1LL * x.second * (rem - x.second);
+            rem -= x.second;
+        }
+        return ans;
+    }
+};
