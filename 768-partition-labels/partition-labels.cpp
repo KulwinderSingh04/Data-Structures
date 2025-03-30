d9c56ed88e7cbe2d85d2889bfcced3068f0dc36c
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-03-30 10:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (71.56%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -4,14 +4,13 @@ public:
         int n = s.size();
         vector<int> hash(26, -1);
         for(int i = n - 1; i >= 0; i--) if(hash[s[i] - 97] == -1) hash[s[i] - 97] = i;
-        set<char> t;
         int i = 0;
         int j = 0;
+        int end = 0;
         vector<int> ans;
         while(j < n) {
-            t.insert(s[j]);
-            if(hash[s[j] - 97] == j) t.erase(s[j]);
-            if(t.size() == 0) {
+            if(end < hash[s[j] - 97]) end = hash[s[j] - 97];
+            if(end == j) {
                 ans.push_back(j - i + 1);
                 i = j + 1;
             }
