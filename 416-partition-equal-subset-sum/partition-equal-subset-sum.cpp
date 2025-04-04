2236adedd3fc236c50da8bd78da6de5be009abb5
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-04 20:29:58 +0530

    Time: 168 ms (21.44%) | Memory: 100.2 MB (45.39%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,10 +4,10 @@ public:
         int n = nums.size();
         if(sum1 / 2 == sum2) return true;
         if(sum1 / 2 < sum2) return false;
-        if(i >= n) return false;
+        if(i <= -1) return false;
         if(dp[i][sum2] != -1) return dp[i][sum2];
-        bool a = fun(nums, i + 1, sum1, sum2 + nums[i], dp);
-        bool b = fun(nums, i + 1, sum1, sum2, dp);
+        bool a = fun(nums, i - 1, sum1, sum2 + nums[i], dp);
+        bool b = fun(nums, i - 1, sum1, sum2, dp);
         return dp[i][sum2] = a || b;
     }
     bool canPartition(vector<int>& nums) {
@@ -16,6 +16,21 @@ public:
         for(int i = 0; i < n; i++) sum1 += nums[i];
         if(sum1 % 2 == 1) return false;
         vector<vector<int>> dp(n, vector<int> (sum1 / 2 + 1, -1));
-        return fun(nums, 0, sum1, sum2, dp);
+        // for(int i = 0; i < n; i++) {
+        //     for(int s = 0; s < sum1 / 2 + 1; s++){
+        //         if(s == 0) dp[i][s] = true;
+        //         else if(i == 0) {
+        //             if(arr[i] == s) dp[i][s] = true;
+        //             else dp[i][s] = false;
+        //         }
+        //         else {
+        //             bool p = false;
+        //             if(s - arr[i] >= 0) p = dp[i - 1][s - arr[i]];
+        //             bool np = dp[i - 1][s];
+        //             dp[i][s] = p || np;
+        //         }
+        //     }
+        // }
+        return fun(nums, n - 1, sum1, sum2, dp);
     }
 };
