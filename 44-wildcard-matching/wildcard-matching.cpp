6be181b024be1f4846bd3fc58a330a8ccf035786
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-10 15:17:10 +0530

    Time: 30 ms (67.88%) | Memory: 31.5 MB (24.84%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool fun(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
-        if(j == -1 && i == -1 ) {
-            return true ;
-        } 
-        if(j == -1 && i >= 0) return false;
-        if(j >= 0 && i == -1) {
-            for(int k = j; k >= 0; k--) {
+    bool fun(string& s, string& p, int i, int j, vector<vector<int>>& dp) {
+        if(i < 0) {
+            for(int k = 0; k <= j; k++) {
                 if(p[k] != '*') return false;
             }
+            cout << "YES" << endl;
             return true;
         }
+        if(j < 0) return false;
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == p[j] || p[j] == '?') return dp[i][j] = fun(i - 1, j - 1, s, p, dp);
-        if(p[j] == '*') {
-            bool a = fun(i - 1, j, s, p, dp);
-            bool b = fun(i, j - 1, s, p, dp);
-            return dp[i][j] = a || b;
-        }
-        return dp[i][j] = false;
+        if(s[i] == p[j] || p[j] == '?') return fun(s, p, i - 1, j - 1, dp);
+        if(s[i] != p[j] && p[j] != '*') return false;
+        bool a = fun(s, p, i - 1, j, dp);
+        bool b = fun(s, p, i, j - 1, dp);
+        return dp[i][j] = a || b;
     }
     bool isMatch(string s, string p) {
-        int i = s.size() - 1;
-        int j = p.size() - 1;
-        vector<vector<int>> dp(s.size(), vector<int> (p.size(), -1));
-        return fun(i, j, s, p, dp);
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return fun(s, p, n - 1, m - 1, dp);
     }
 };
