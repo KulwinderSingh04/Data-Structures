588b94c8282baf1e4a544937734f333e0c896c9f
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-10 15:40:14 +0530

    Time: 39 ms (26.80%) | Memory: 31.7 MB (18.85%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -5,7 +5,6 @@ public:
             for(int k = 0; k <= j; k++) {
                 if(p[k] != '*') return false;
             }
-            cout << "YES" << endl;
             return true;
         }
         if(j < 0) return false;
@@ -19,7 +18,26 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return fun(s, p, n - 1, m - 1, dp);
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        for(int j = 0; j <= m; j++) {
+            for(int i = 1; i <= j; i++) {
+                if(p[i - 1] != '*') {
+                    dp[0][j] = false;
+                    break;
+                }
+            }
+            if(dp[0][j] == -1) dp[0][j] = true;
+        }
+        for(int i = 1; i < n + 1; i++) dp[i][0] = false;
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 1; j < m + 1; j++) {
+                if(s[i - 1] == p[j - 1] || p[j - 1] == '?') dp[i][j] = dp[i - 1][j - 1];
+                else if(s[i - 1] != p[j - 1] && p[j - 1] != '*') dp[i][j] = false;
+                else {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
