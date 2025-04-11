a5dc9dca51e0e9f6dd40bcaa495593b4ebd3f879
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-11 15:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (62.71%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
-    int fun(vector<int>& prices, int i, int buy, vector<vector<int>>& dp) {
-        if(i == prices.size() - 1) {
-            if(buy) return prices[i];
-            return 0;
-        }
-        if(dp[i][buy] != -1) return dp[i][buy];
-        if(buy == 0) {
-            int a = -prices[i] + fun(prices, i + 1, 1, dp);
-            int b = fun(prices, i + 1, 0, dp);
-            return dp[i][buy] = max(a, b);
-        }
-        else {
-            int a = prices[i] + fun(prices, i + 1, 0, dp);
-            int b = fun(prices, i + 1, 1, dp);
-            return dp[i][buy] = max(a, b);
-        }
-    }
     int maxProfit(vector<int>& prices) {
+        int mn = prices[0];
+        int ans = 0;
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int> (2, 0));
-        dp[n - 1][1] = prices[n - 1];
-        dp[n - 1][0] = 0;
-        for(int i = n - 2 ; i >= 0; i--) {
-            for(int j = 1; j >= 0; j--) {
-                if(j == 0) {
-                    int a = -prices[i] + dp[i + 1][1];
-                    int b = dp[i + 1][0];
-                    dp[i][j] = max(a, b);
-                }
-                else {
-                    int a = prices[i] + dp[i + 1][0];
-                    int b = dp[i + 1][1];
-                    dp[i][j] = max(a, b);
-                }
+        for(int i = 0 ; i < n; i++) {
+            if(prices[i] > mn) {
+                ans += prices[i] - mn;
+                mn = prices[i];
             }
+            mn = min(mn, prices[i]);
         }
-        return dp[0][0];
+        return ans;
     }
 };
