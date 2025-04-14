448ffca2fcf55c8d7d65713ddc933d99235bece5
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-14 11:26:21 +0530

    Time: 4 ms (90.51%) | Memory: 11.5 MB (5.91%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int count = 0;
+        int ans = 0;
         int n = arr.size();
-        for(int i = 0; i < n - 2; i++) {
-            for(int j = i + 1; j < n - 1; j++) {
-                if(abs(arr[i] - arr[j]) > a) continue;
-                for(int k = j + 1; k < n; k++) {
-                    if(abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c) count++;
+        vector<int> pref(1001);
+        for(int j = 0; j < n; j++) {
+            for(int k = j + 1; k < n; k++) {
+                if(abs(arr[j] - arr[k]) > b) continue; 
+                int r = min(1000, min(arr[j] + a, arr[k] + c));
+                int l = max(0, max(arr[j] - a, arr[k] - c));
+                if(l <= r) {
+                    if(l != 0) ans += pref[r] - pref[l - 1];
+                    else ans += pref[r];
                 }
             }
+            for(int idx = arr[j]; idx < 1001; idx++) pref[idx]++;
         }
-        return count;
+        return ans;
     }
 };
