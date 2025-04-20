c1faadf5cd2ab095772b2f3aae3379224d4e7f5c
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-20 13:20:51 +0530

    Time: 1510 ms (38.95%) | Memory: 9.5 MB (67.83%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -5,23 +5,22 @@ public:
         while(i < j) if(s[i++] != s[j--]) return false;
         return true;
     }
-    int fun(string& s, int i, int j, vector<vector<int>>& dp) {
-        if(i > j) return 0;
-        // if(i == j) return 0;
+    int fun(string& s, int i, int n, vector<int>& dp) {
+        if(i == n) return 0;
         int mn = INT_MAX;
-        if(dp[i][j] != -1) return dp[i][j];
-        for(int k = i; k <= j; k++) {
+        if(dp[i] != -1) return dp[i];
+        for(int k = i; k < n; k++) {
             int steps = INT_MAX;
             if(isPal(s, i, k)) {
-                steps = 1 + fun(s, k + 1, j, dp);
+                steps = 1 + fun(s, k + 1, n, dp);
                 mn = min(mn, steps);
             }
         }
-        return dp[i][j] = mn;
+        return dp[i] = mn;
     }
     int minCut(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return fun(s, 0, n - 1, dp) - 1;
+        vector<int> dp(n, -1);
+        return fun(s, 0, n, dp) - 1;
     }
 };
