c8b1d9b14e7dea2c65e6745233ec9a67653477ae
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-21 09:43:37 +0530

    Time: 10 ms (13.71%) | Memory: 122.6 MB (5.64%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -4,19 +4,13 @@ public:
         int n = differences.size();
         vector<long long> v(n + 1);
         v[n] = upper;
-        long long mx = upper;
+        long long mx = upper, mn = v[n];
         for(int i = n - 1; i >= 0; i--) {
             v[i] = v[i + 1] - differences[i];
             mx = max(mx, v[i]);
-        }
-        mx = mx - upper;
-        long long mn = INT_MAX;
-        for(int i = 0; i < v.size(); i++) {
-            v[i] -= mx;
             mn = min(mn, v[i]);
         }
+        mn -= (mx - upper);
         return mn - lower < 0 ? 0 : mn - lower + 1;
-        // int ans = ceil(v, lower, upper) - floor(v, lower, upper);
-        // return ans < 0 ? 0 : ans + 1;
     }
 };
