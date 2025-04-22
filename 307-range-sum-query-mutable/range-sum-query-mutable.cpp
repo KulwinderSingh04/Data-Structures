e3304028782f9399c5fc937546e0d7816dfbde25
KulwinderSingh04 <kulwindersingh157679@gmail.com>
2025-04-22 10:09:42 +0530

    Time: 71 ms (42.94%) | Memory: 182.4 MB (28.06%) - LeetSync

---
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -0,0 +1,58 @@
+class NumArray {
+public:
+    vector<int> segTree;
+    int n;
+
+    void buildTree(vector<int>& nums, int i, int l, int r) {
+        if(l == r) {
+            segTree[i] = nums[l];
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        buildTree(nums, 2 * i + 1, l, mid);
+        buildTree(nums, 2 * i + 2, mid + 1, r);
+        segTree[i] = segTree[2 * i + 1] + segTree[2 * i + 2];
+    }
+
+    int query(int left, int right, int i, int l, int r) {
+        if(r < left || l > right) return 0;
+        if(l >= left && r <= right) return segTree[i];
+        int mid = l + (r - l) / 2;
+        return query(left, right, 2 * i + 1, l, mid) + query(left, right, 2 * i + 2, mid + 1, r);
+    }
+
+    void segUpdate(int idx, int val, int i, int l, int r) {
+        if(l == r && idx == l) {
+            segTree[i] = val;
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if(idx >= l && idx <= mid) {
+            segUpdate(idx, val, 2 * i + 1, l, mid);
+        }
+        else {
+            segUpdate(idx, val, 2 * i + 2, mid + 1, r); 
+        }
+        segTree[i] = segTree[2 * i + 1] + segTree[2 * i + 2];
+    }
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        segTree.resize(4 * n);
+        buildTree(nums, 0, 0, n - 1);
+    }
+    
+    void update(int index, int val) {
+        segUpdate(index, val, 0, 0, n - 1);
+    }
+    
+    int sumRange(int left, int right) {
+        return query(left, right, 0, 0, n - 1);
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
