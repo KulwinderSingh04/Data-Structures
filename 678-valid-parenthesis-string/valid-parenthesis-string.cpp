5632d30acf12e852f1d2ae6271d844dfe695cccf
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-04-23 18:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (50.14%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    bool fun(string& s, int i, int o, int c, vector<vector<vector<int>>>& dp) {
-        int n = s.size();
-        if(c > o) return false;
-        
-        if(i >= n) return c == o;
-        if(dp[i][o][c] != -1) return dp[i][o][c];
-        if(s[i] == '(') return dp[i][o][c] = fun(s, i + 1, o + 1, c, dp);
-        else if(s[i] == ')') return dp[i][o][c] = fun(s, i + 1, o, c + 1, dp);
-        else {
-            bool x = fun(s, i + 1, o , c, dp);
-            bool y = fun(s, i + 1, o + 1 , c, dp);
-            bool z = fun(s, i + 1, o , c + 1, dp);
-            return dp[i][o][c] = x || y || z;
-        }
-    }
     bool checkValidString(string s) {
-        int n = s.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (n, vector<int> (n, -1)));
-        return fun(s, 0, 0, 0, dp);
+        int mn = 0;
+        int mx = 0;
+        for(int i = 0; i < s.size(); i++) {
+            if(s[i] == '(') {
+                mn++;
+                mx++;
+            } else if(s[i] == ')') {
+                mn--;
+                mx--;
+            } else {
+                mn--;
+                mx++;
+            }
+            if(mn < 0) mn = 0;
+            if(mx < 0) return false;
+        }
+        return mn == 0;
     }
 };
