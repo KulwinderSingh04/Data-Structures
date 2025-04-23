a618f68a7b17903747e89b3ae613ec612125ee5d
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-04-23 19:42:18 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (87.72%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int mx = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            if(mx < i) return false;
+            mx = max(mx, nums[i] + i);
+        }
+        return true;
+    }
+};
