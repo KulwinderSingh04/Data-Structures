423e9ed945459601b5009e7ce50fcfbb634c53f0
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-04-25 12:56:51 +0530

    Time: 63 ms (42.08%) | Memory: 122.8 MB (59.02%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -4,15 +4,13 @@ public:
         int n = nums.size();
         long long count = 0;
         long long ans = 0;
-        int t = 0;
         unordered_map<int, int> m;
         m[0] = 1;
         for(int i = 0; i < n; i++) {
             int rem = nums[i] % modulo;
             if(rem == k) count++;
-            t = count;
-            ans += m[(t - k) % modulo];
-            m[t % modulo]++;
+            ans += m[(count - k) % modulo];
+            m[count % modulo]++;
         }
         return ans;
 
