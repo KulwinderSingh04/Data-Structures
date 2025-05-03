93e6b7bfdeb3f221e1bafbabc29677ac458dadbf
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-03 08:02:18 +0530

    Time: 8 ms (29.53%) | Memory: 115.4 MB (76.51%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -2,22 +2,28 @@ class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
         int n = tops.size();
-        map<int, int> m1, m2;
-        int ele1 = 0, ele2 = 0;;
+        int ele1 = 0, ele2 = 0;
+        int c1 = 0, c2 = 0;
         for(int i = 0; i < n; i++) {
-            m1[tops[i]]++;
-            if(m1[tops[i]] >= n / 2) ele1 = tops[i];
-
-            m2[bottoms[i]]++;
-            if(m2[bottoms[i]] >= n / 2) ele2 = bottoms[i];
-
+            if(c1 == 0) {
+                ele1 = tops[i];
+                c1 = 0;
+            }
+            if(ele1 != tops[i]) c1--;
+            else c1++;
+            
+            if(c2 == 0) {
+                ele2 = bottoms[i];
+                c2 = 0;
+            }
+            if(ele2 != bottoms[i]) c2--;
+            else c2++;
         }
-        cout << ele1 << " " << ele2 << endl;
-        int ans = -1, count = 0;
+        int ans = INT_MAX, count = 0;
         for(int i = 0; i < n; i++) {
             if(tops[i] != ele1) {
                 if(bottoms[i] != ele1) {
-                    count = -1;
+                    count = INT_MAX;
                     break;
                 } else count++;
             }
@@ -27,12 +33,13 @@ public:
         for(int i = 0;i < n; i++) {
             if(bottoms[i] != ele2) {
                 if(tops[i] != ele2) {
-                    count = -1;
+                    count = INT_MAX;
                     break;
                 } else count++;
             }
         }
-        if(ans == -1) return count; 
-        return min(ans, count);
+        ans = min(ans, count);
+        if(ans == INT_MAX) return -1;
+        return ans;
     }
 };
