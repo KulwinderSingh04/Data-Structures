e5e787ada5ccd1a6b377aa4bb69725c563f8f8cc
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-16 18:37:38 +0530

    Time: 8 ms (62.47%) | Memory: 11.1 MB (58.75%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        int n = s.size();
         int i = 0;
         int j = 0;
         int ans = 0;
         vector<int> m(3);
-        int n = s.size();
         while(j < n) {
-            m[s[j] - 97]++;
+            m[s[j] - 'a']++;
             while(m[0] > 0 && m[1] > 0 && m[2] > 0) {
                 ans += n - j;
-                m[s[i] - 97]--;
+                m[s[i] - 'a']--;
                 i++;
             }
             j++;
