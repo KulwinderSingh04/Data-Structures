9d85fd8fcbede24f6d9032b04ab2bfe401882be1
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-21 08:09:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (88.83%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,8 +5,6 @@ public:
         for(int i = 0; i < numRows; i++) {
             vector<int> a(i + 1);
             v.push_back(a);
-        }
-        for(int i = 0; i < numRows; i++) {
             for(int j = 0; j < i + 1; j++) {
                 if(j == 0 || j == i) v[i][j] = 1;
                 else v[i][j] = v[i - 1][j] + v[i - 1][j - 1];
