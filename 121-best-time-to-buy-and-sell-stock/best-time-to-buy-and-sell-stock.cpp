7201b8876799b6870fad0e69a4e993dc725078ff
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-21 08:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (61.38%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 0;
         int mn = prices[0];
         int n = prices.size();
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i < n; i++) {
             ans = max(ans, prices[i] - mn);
             mn = min(mn, prices[i]);
         }
