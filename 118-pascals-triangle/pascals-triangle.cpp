5d2f7671f52ce8d5be120771577f393d55f942e6
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-21 08:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (98.93%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    int MOD = 1e9 + 7;
+    int com(int n, int r) {
+        long long res = 1;
+        for(int i = n; i > n - r; i--) {
+            res *= i;
+            if(n - i + 1 <= r) res /= n - i + 1;
+        }
+        return res;
+    }
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> v;
         for(int i = 0; i < numRows; i++) {
             vector<int> a(i + 1);
             v.push_back(a);
             for(int j = 0; j < i + 1; j++) {
-                if(j == 0 || j == i) v[i][j] = 1;
-                else v[i][j] = v[i - 1][j] + v[i - 1][j - 1];
+                v[i][j] = com(i, j);
             }
         }
         return v;
