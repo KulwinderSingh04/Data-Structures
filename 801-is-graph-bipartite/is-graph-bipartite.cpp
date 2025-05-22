8f2445163e71e17f0cb39e4731f6cc3f252d8460
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-22 11:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (34.37%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -4,7 +4,6 @@ public:
         colArr[i] = color;
         for(auto t : graph[i]) {
             if(colArr[t] == -1) {
-                // colArr[t] = !color;
                 bool a = dfs(graph, t, !color, colArr);
                 if(a == false) return a;
             }
