0b32d524be856fb668530fda9d531f8db5390015
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-22 10:35:06 +0530

    Time: 19 ms (18.86%) | Memory: 17.8 MB (21.89%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    bool fun(int idx, string& s, unordered_set<string>& dict, vector<int>& dp) {
-        int n = s.size();
-        if(idx == n) return true;
-        if(dp[idx] != -1) return dp[idx];
-        for(int i = idx; i < s.size(); i++) {
-            string a = s.substr(idx, i - idx + 1);
-            if(dict.find(a) != dict.end()) {
-                bool f = fun(i + 1, s, dict, dp);
-                if(f) return dp[idx] = f;
-            }
-        }
-        return dp[idx] = false;
-    }
+    // bool fun(int idx, string& s, unordered_set<string>& dict, vector<int>& dp) {
+    //     int n = s.size();
+    //     if(idx == n) return true;
+    //     if(dp[idx] != -1) return dp[idx];
+    //     for(int i = idx; i < s.size(); i++) {
+    //         string a = s.substr(idx, i - idx + 1);
+    //         if(dict.find(a) != dict.end()) {
+    //             bool f = fun(i + 1, s, dict, dp);
+    //             if(f) return dp[idx] = f;
+    //         }
+    //     }
+    //     return dp[idx] = false;
+    // }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> dict;
         int n = s.size();
