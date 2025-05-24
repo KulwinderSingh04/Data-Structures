eea59ad46c22cb9a98a6e3332aee87dd88795f54
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-24 12:46:17 +0530

    Time: 16 ms (70.16%) | Memory: 24.8 MB (67.17%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     typedef pair<int, pair<int, int>> pp;
-    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid[0][0] == 1) return -1;
-        int n = grid.size();
-        queue<pp> q;
-        q.push({1, {0, 0}});
-        grid[0][0] = 1;
+    int shortestPathBinaryMatrix(vector<vector<int>>& mat) {
         int ans = -1;
-        // vector<vector<int>> vis(n, vector<int> (n));
-        vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, 1}, {1, -1}, {-1, -1}, {1, 1}};
+        int n = mat.size();
+        queue<pair<int, pair<int, int>>> q;
+        if(mat[0][0] == 0) {
+            q.push({1, {0, 0}});
+            mat[0][0] = 1;
+        }
+        vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
         while(q.size()) {
             auto t = q.front();
             q.pop();
+            int dis = t.first;
             int r = t.second.first;
             int c = t.second.second;
-            if(r == n - 1 && c == n - 1) {
-                ans = t.first;
-                break;
-            }
+            if(r == n - 1 && c == n - 1) return dis;
             for(int i = 0; i < 8; i++) {
                 int nr = r + dir[i][0];
                 int nc = c + dir[i][1];
-                if(nr < 0 || nc < 0 || nr >= n || nc >= n || grid[nr][nc] == 1) continue;
-                grid[nr][nc] = 1;
-                q.push({t.first + 1, {nr, nc}});
+                if(nr < 0 || nr >= n || nc < 0 || nc >= n || mat[nr][nc] == 1) continue;
+                q.push({dis + 1,{nr, nc}});
+                mat[nr][nc] = 1;
             }
         }
         return ans;
