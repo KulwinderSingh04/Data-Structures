1476ba282ba92570e0df4f1e59a30f028475dc55
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-29 22:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (64.99%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int i = 0; i < 32; i++) {
-            int count = 0;
-            for(int j = 0; j < nums.size(); j++) {
-                if((nums[j] & (1 << i)) != 0) count++;
-            }
-            if(count % 3 == 1) ans |= (1 << i);
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int i = 1;
+        while(i < n) {
+            if(nums[i] != nums[i - 1]) return nums[i - 1];
+            i += 3;
         }
-        return ans;
+        return nums.back();
     }
 };
