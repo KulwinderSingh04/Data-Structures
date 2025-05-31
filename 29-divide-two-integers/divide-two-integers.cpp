e53ebc609084fc5714886e34d97d5d2b4f92034d
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-05-31 18:36:55 +0530

    Time: 7 ms (7.17%) | Memory: 8.7 MB (37.68%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        long long a = dividend / (divisor * 1LL);
-        if(a > INT_MAX) return INT_MAX;
-        if(a < INT_MIN) return INT_MIN;
-        return a;
+    int divide(int n, int d) {
+        long long ans = 0;
+        int sign = 1;
+        if(n < 0 && d >= 0) sign = -1;
+        if(n >= 0 && d < 0) sign = -1;
+        cout << sign << endl;
+        long long n1 = n;
+        long long d1 = d;
+        n1 = abs(n1);
+        d1 = abs(d1);
+        while(n1 >= d1) {
+            long long cnt = 0;
+            while(n1 >= (1LL * (d1 << (cnt + 1)))) cnt++;
+            ans += (1 << cnt);
+            n1 -= (d1 << cnt);
+        }
+        if(ans == (1 << 31) && sign == 1) return INT_MAX;
+        if(ans == (1 << 31) && sign == -1) return INT_MIN;
+        if(sign == -1) ans *= -1;
+        return ans;
     }
 };
