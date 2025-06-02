78f3d33cc214bc5dd6fa8fa8ec2ab254f60ff7fd
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-02 09:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (27.40%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,34 +2,22 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> v(n);
-        v[0] = 1;
-        int mn = INT_MAX;
+        vector<int> left(n, 0);
+        left[0] = 1;
+        int count = 1;
         for(int i = 1; i < n; i++) {
             if(ratings[i] > ratings[i - 1]) {
-                v[i] = v[i - 1] + 1;
-            }
-            else {
-                v[i] = 1;
-            }
-        }
-        vector<int> v2(n, 1);
-        v[n - 1] = max(v[n - 1], v2[n - 1]);
-        for(int i = n - 2; i >= 0; i--) {
-            if(ratings[i] > ratings[i + 1]) {
-                v2[i] = v2[i + 1] + 1;
-            }
-            else {
-                v2[i] = 1;
-            }
-            v[i] = max(v[i], v2[i]);
-            
-            mn = min(mn, v[i]);
+                count++;
+            } else count = 1;
+            left[i] = count;
         }
         int ans = 0;
-        for(int i = 0; i < n; i++) {
-            if(mn < 1) v[i] += 1 - mn;
-            ans += v[i];
+        for(int i = n - 1; i >= 0; i--) {
+            if(i + 1 < n && ratings[i] > ratings[i + 1]) {
+                count++;
+            } else count = 1;
+            left[i] = max(left[i], count);
+            ans += left[i];
         }
         return ans;
     }
