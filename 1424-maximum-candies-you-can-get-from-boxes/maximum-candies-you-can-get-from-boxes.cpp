b94a8441d2e22e0e576b9311d47d2a7dcf39b689
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-03 08:19:39 +0530

    Time: 1 ms (76.44%) | Memory: 44.8 MB (31.25%) - LeetSync

---
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int n = status.size();
+        queue<int> q;
+        unordered_set<int> closedBoxes;
+        
+        vector<int> vis(n);
+        for(auto x : initialBoxes) {
+            if(status[x] == 1) {
+                q.push(x);
+                status[x] = 0;
+                vis[x] = 1;
+            }
+            else closedBoxes.insert(x);
+        }
+        // unordered_map<int, int> m;
+        int ans = 0;
+        while(q.size()) {
+            auto t = q.front();
+            q.pop();
+            ans += candies[t];
+            for(auto x : keys[t]) {
+                status[x] = 1;
+            }
+            for(auto x : containedBoxes[t]) {
+                if(vis[x] == 1) continue;
+                if(status[x] == 1) {
+                    q.push(x);
+                    status[x] = 0;
+                    vis[x] = 1;
+                }
+                else closedBoxes.insert(x);
+            }
+            // vector<int> v;
+            for(auto x : closedBoxes) {
+                if(vis[x] == 1) continue;
+                if(status[x] == 1) {
+                    q.push(x);
+                    // v.push_back(x);
+                    status[x] = 0;
+                    vis[x] = 1;
+                }
+            }
+            // for(auto x : v) closedBoxe
+        }
+        return ans;
+    }
+};
