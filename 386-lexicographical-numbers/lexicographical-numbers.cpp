d0459459520399e7ca52560d992f6d6e8881f7b6
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-08 08:51:20 +0530

    Time: 54 ms (19.29%) | Memory: 36.8 MB (5.05%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,15 +1,45 @@
+class Node {
+    public:
+    bool flag;
+    vector<Node*> links;
+    Node() {
+        flag = false;
+        links.resize(10, NULL);
+    }
+};
 class Solution {
+    Node* root;
 public:
-    vector<int> lexicalOrder(int n) {
-        vector<string> a;
-        for(int i = 1; i < n + 1; i++) {
-            a.push_back(to_string(i));
-        }
-        sort(a.begin(), a.end());
-        vector<int> ans;
+    void insert(string word) {
+        int n = word.size();
+        Node* node = root;
         for(int i = 0; i < n; i++) {
-            ans.push_back(stoi(a[i]));
+            if(node -> links[word[i] - 48] == NULL) {
+                node -> links[word[i] - 48] = new Node();
+            }
+            node = node -> links[word[i] - 48];
         }
-        return ans;
+        node -> flag = true;
+    }
+    void dfs(Node* node, string s, vector<int>& v) {
+        if(node -> flag) {
+            v.push_back(stoi(s));
+        }
+        for(int i = 0; i < 10; i++) {
+            if(node -> links[i]) {
+                s += i + 48;
+                dfs(node -> links[i], s, v);
+                s.pop_back();
+            }
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+        root = new Node();
+        for(int i = 1; i < n + 1; i++) {
+            insert(to_string(i));
+        }
+        vector<int> v;
+        dfs(root, "", v);
+        return v;
     }
 };
