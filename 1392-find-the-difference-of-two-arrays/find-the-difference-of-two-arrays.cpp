6e1a5a42a745c87d9c5b3c0d84b9b03732510f6a
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-08 14:55:58 +0530

    Time: 19 ms (73.55%) | Memory: 36.9 MB (76.20%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        unordered_set<int> s1, s2;
-        for(auto x : nums1) s1.insert(x);
-        for(auto x : nums2) s2.insert(x);
         vector<vector<int>> ans(2);
-        for(int i = 0; i < n1; i++) {
-            if(s2.find(nums1[i]) == s2.end()) {
-                ans[0].push_back(nums1[i]);
-            }
-            s2.insert(nums1[i]);
+        unordered_set<int> s1;
+        unordered_set<int> s2;
+        for(auto i:nums1)
+        {
+            s1.insert(i);
         }
-        for(int i = 0; i < n2; i++) {
-            if(s1.find(nums2[i]) == s1.end()) ans[1].push_back(nums2[i]);
-            s1.insert(nums2[i]);
+        for(auto i:nums2)
+        {
+            s2.insert(i);
+        }
+        for(auto i:s1)
+        {
+            if(s2.find(i) ==s2.end())
+            {
+                ans[0].push_back(i);
+            }
+        }
+        for(auto i:s2)
+        {
+            if(s1.find(i) ==s1.end())
+            {
+                ans[1].push_back(i);
+            }
         }
         return ans;
     }
