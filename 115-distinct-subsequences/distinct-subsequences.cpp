7119e9f2f780dee9f9cccb58a1082d800880f4d6
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-12 11:31:05 +0530

    Time: 27 ms (71.13%) | Memory: 44.3 MB (32.09%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int fun(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if(i == s.size()) {
-            return j == t.size();
-        }
-        if(j == t.size()) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j]) {
-            int a = fun(i + 1, j + 1, s, t, dp);
-            int b = fun(i + 1, j, s, t, dp);
-            return dp[i][j] = a + b;
-        }
-        int a = fun(i + 1, j, s, t, dp);
-        return dp[i][j] = a;
-    }
+    // int fun(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+    //     if(i == -1) {
+    //         return j == -1;
+    //     }
+    //     if(j == -1) return 1;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     if(s[i] == t[j]) {
+    //         int a = fun(i - 1, j - 1, s, t, dp);
+    //         int b = fun(i - 1, j, s, t, dp);
+    //         return dp[i][j] = a + b;
+    //     }
+    //     int a = fun(i - 1, j, s, t, dp);
+    //     return dp[i][j] = a;
+    // }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return fun(0, 0, s, t, dp);
+        vector<vector< unsigned long long>> dp(n + 1, vector< unsigned long long> (m + 1, 0));
+        // for(int )
+        dp[0][0] = 1;
+        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(s[i - 1] == t[j - 1]) {
+                    unsigned long long a = dp[i - 1][j - 1];
+                    unsigned long long b = dp[i - 1][j];
+                    dp[i][j] = a + b;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
