edbe96117cf73d69cce3998ee74467c6f3cce4c3
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-12 11:22:07 +0530

    Time: 43 ms (14.47%) | Memory: 26.9 MB (72.47%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int fun(string& s, string& t, int i, int j, vector<vector<int>>& dp) {
-        int n = s.size();
-        int m = t.size();
-        if(j < 0) return 1;
-        if(i < 0) return 0;
+    int fun(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if(i == s.size()) {
+            return j == t.size();
+        }
+        if(j == t.size()) return 1;
         if(dp[i][j] != -1) return dp[i][j];
-        int a = 0;
-        if(s[i] == t[j]) a = fun(s, t, i - 1, j - 1, dp); 
-        int b = fun(s, t, i - 1, j, dp);
-        return dp[i][j] = a + b;
+        if(s[i] == t[j]) {
+            int a = fun(i + 1, j + 1, s, t, dp);
+            int b = fun(i + 1, j, s, t, dp);
+            return dp[i][j] = a + b;
+        }
+        int a = fun(i + 1, j, s, t, dp);
+        return dp[i][j] = a;
     }
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long> (m + 1, 0));
-        for(int i = 0;  i < n; i++) dp[i][0] = 1;
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                unsigned long long a = 0;
-                if(s[i - 1] == t[j - 1]) a = dp[i - 1][j - 1]; 
-                unsigned long long b = dp[i - 1][j];
-                dp[i][j] = a + b;
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return fun(0, 0, s, t, dp);
     }
 };
