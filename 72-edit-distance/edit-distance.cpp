b0f4b9e671c02f85b00758e1fd6a651c90c0bd1f
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-13 15:44:55 +0530

    Time: 8 ms (46.12%) | Memory: 12.8 MB (83.07%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int fun(string& s, string& t, int i, int j, vector<vector<int>>& dp) {
-        if(i < 0) return j + 1;
-        if(j < 0) return i + 1;
+    int fun(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
+        if(i == -1 && j == -1) {
+            return 0;
+        }
+        if(i == -1) {
+            return j + 1;
+        }
+        if(j == -1) return i + 1;
         if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == t[j]) return dp[i][j] = fun(s, t, i - 1, j - 1, dp);
-        int x = 1 + fun(s, t, i, j - 1, dp);
-        int y = 1 + fun(s, t, i - 1, j, dp);
-        int z = 1 + fun(s, t, i - 1, j - 1, dp);
-        return dp[i][j] = min(x, min(y, z));
+        if(s1[i] == s2[j]) return dp[i][j] = fun(i - 1, j - 1, s1, s2, dp);
+        else {
+            int a = fun(i - 1, j - 1, s1, s2, dp);
+            int b = fun(i - 1, j, s1, s2, dp);
+            int c = fun(i, j - 1, s1, s2, dp);
+            return dp[i][j] = 1 + min(a, min(b, c));
+        }
     }
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
-        for(int i = 0; i <= n; i++) dp[i][0] = i;
-        for(int i = 0; i <= m; i++) dp[0][i] = i;
-        for(int i = 1; i < n + 1; i++) {
-            for(int j = 1; j  < m + 1; j++) {
-                if(word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
-                else dp[i][j] = 1 + min(dp[i - 1][j], min(dp[i][j - 1], dp[i - 1][j - 1]));
-            }
-        }
-        return dp[n][m];
+        int n1 = word1.size();
+        int n2 = word2.size();
+        vector<vector<int>> dp(n1, vector<int> (n2, -1));
+        return fun(n1 - 1, n2 - 1, word1, word2, dp);
     }
 };
