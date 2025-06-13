79e4ca278daa004a6d3c2cb3f3eec3b84295907a
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-13 14:56:19 +0530

    Time: 41 ms (26.43%) | Memory: 27.6 MB (14.68%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
-    int fun(string text1, string& text2, int i, int j, vector<vector<int>>& dp) {
-        if(i == 0 || j == 0) {
-            if(j == 0) {
-                for(int k = 0; k <= i; k++) {
-                    if(text1[k] == text2[j]) return 1;
-                }
-            }
-            else {
-                for(int k = 0; k <= j; k++) {
-                    if(text2[k] == text1[i]) return 1;
-                }
-            }
-            return 0;
-        }
+    int fun (int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
+        if(i == -1 || j == -1) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(text1[i] == text2[j]) return 1 + fun(text1, text2, i - 1, j - 1, dp);
-        return dp[i][j] = max(fun(text1, text2, i - 1, j, dp), fun(text1, text2, i , j - 1, dp));
+        if(s1[i] == s2[j]) return dp[i][j] = 1 + fun(i - 1, j - 1, s1, s2, dp);
+        int a = fun(i - 1, j, s1, s2, dp);
+        int b = fun(i, j - 1, s1, s2, dp);
+        return dp[i][j] = max(a, b);
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size();
         int n2 = text2.size();
-        vector<vector<int>> dp(n1, vector<int> (n2, 0));
-        for(int i = 0; i < n1; i++) {
-            for(int k = 0; k <= i; k++) {
-                if(text1[k] == text2[0]) dp[i][0] = 1;
-            }
-        }
-        for(int j = 0; j < n2; j++) {
-            for(int k = 0; k <= j; k++) {
-                if(text2[k] == text1[0]) dp[0][j] = 1;
-            }
-        }
-        for(int i = 1; i < n1; i++) {
-            for(int j = 1; j < n2; j++) {
-                if(text1[i] == text2[j]) dp[i][j] = 1 + dp[i - 1][j - 1];
-                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-        return dp[n1 - 1][n2 - 1];
+        vector<vector<int>> dp(n1, vector<int> (n2, -1));
+        return fun(n1 - 1, n2 - 1, text1, text2, dp);
     }
 };
