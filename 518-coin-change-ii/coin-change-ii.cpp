5b162f8915533a8a74af6e316a0d4ead65be6f3f
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-17 16:21:35 +0530

    Time: 34 ms (20.49%) | Memory: 28.6 MB (39.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int fun(vector<int>& coins, int i, int amount, vector<vector<int>>& dp) {
-        if(i == 0) {
-            if(amount % coins[i] == 0) return 1;
-            return 0;
-        }
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int p = 0;
-        if(amount - coins[i] >= 0) p = fun(coins, i, amount - coins[i], dp);
-        int np = fun(coins, i - 1, amount, dp);
-        return dp[i][amount] = p + np;
+    int fun(int idx, int sum, vector<int>& nums, vector<vector<int>>& dp) {
+        int n = nums.size();
+        if(idx == n)  {
+            return sum == 0;
+        }  
+        if(dp[idx][sum] != -1) return dp[idx][sum];
+        long long a = 0;
+        if(sum - nums[idx] >= 0) a = fun(idx, sum - nums[idx], nums, dp);
+        long long b = fun(idx + 1,  sum, nums, dp);
+        return dp[idx][sum] = a + b;
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<uint64_t>> dp(n, vector<uint64_t> (amount + 1, -1));
-        for(int a = 0; a < amount + 1; a++) {
-            if(a % coins[0] == 0) dp[0][a] = 1;
-            else dp[0][a] = 0; 
-        }
-        for(int i = 1; i < n; i++) {
-            for(int a = 0; a < amount + 1; a++) {
-                uint64_t p = 0;
-                if(a - coins[i] >= 0) p = dp[i][a - coins[i]];
-                uint64_t np = dp[i - 1][a];
-                dp[i][a] = p + np;
-            }
-        }
-        return dp[n - 1][amount];
+        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
+        return fun(0, amount, coins, dp);
     }
 };
