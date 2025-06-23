f3ee5cc65b87b8af1d66f1d5a8b676b9cc7ca2e7
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-23 16:19:38 +0530

    Time: 457 ms (17.87%) | Memory: 223.8 MB (27.26%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,50 +1,23 @@
 class Solution {
 public:
-    int fun(vector<int>& prices, int i, int buy, int cap, vector<vector<vector<int>>>& dp) {
-        if(cap == 0) return 0;
-        if(i == prices.size() - 1) {
-            if(buy) return prices[i];
-            return 0;
-        }
-        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
+    int fun(int i, int buy, int k, vector<int>& prices, vector<vector<vector<int>>>& dp) {
+        if(i == prices.size()) return 0;
+        if(k == 0) return 0;
+        if(dp[i][buy][k] != -1) return dp[i][buy][k];
         if(buy == 0) {
-            int a = -prices[i] + fun(prices, i + 1, 1, cap, dp);
-            int b = fun(prices, i + 1, 0, cap, dp);
-            return dp[i][buy][cap] = max(a, b);
-        }
-        else {
-            int a = prices[i] + fun(prices, i + 1, 0, cap - 1, dp);
-            int b = fun(prices, i + 1, 1, cap, dp);
-            return dp[i][buy][cap] = max(a, b);
+            int a = -prices[i] + fun(i + 1, !buy, k, prices, dp);
+            int b = fun(i + 1, buy, k, prices, dp);
+            return dp[i][buy][k] = max(a, b);
+        } else {
+            int a = prices[i] + fun(i + 1, !buy, k - 1, prices, dp);
+            int b = fun(i + 1, buy, k, prices, dp);
+            return dp[i][buy][k] = max(a, b);
         }
     }
-    int maxProfit(vector<int>& prices) {
-        int cap = 2;
+    int maxProfit(vector<int> &prices) {
+        // code here
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (cap + 1, 0)));
-        for(int i = n - 1 ; i >= 0; i--) {
-            for(int j = 1; j >= 0; j--) {
-                for(int k = 0; k <= cap; k++) {
-                    if(k == 0) dp[i][j][k] = 0;
-                    else if(i == n - 1) {
-                        if(j == 1) dp[i][j][k] = prices[i];
-                        else dp[i][j][k] = 0;
-                    }
-                    else {
-                        if(j == 0) {
-                            int a = -prices[i] + dp[i + 1][1][k];
-                            int b = dp[i + 1][0][k];
-                            dp[i][j][k] = max(a, b);
-                        }
-                        else {
-                            int a = prices[i] + dp[i + 1][0][k - 1];
-                            int b = dp[i + 1][1][k];
-                            dp[i][j][k] = max(a, b);
-                        }
-                    }
-                }
-            }
-        }
-        return dp[0][0][cap];
+        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (3, -1)));
+        return fun(0, 0, 2, prices, dp);
     }
 };
