87c8b8dcdfdfadfd035fa43d5b04dc4f60ea0957
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-24 07:43:49 +0530

    Time: 2 ms (63.67%) | Memory: 14.6 MB (90.46%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        vector<int> v;
+        // vector<int> v;
         int n = nums.size();
-        for(int i = 0; i < n; i++) if(nums[i] == key) v.push_back(i);
-        sort(v.begin(), v.end());
+        // for(int i = 0; i < n; i++) if(nums[i] == key) v.push_back(i);
+        // sort(v.begin(), v.end());
         int curr = 0;
+        // while(curr < n && nums[curr] != key) curr++;
         vector<int> ans;
         for(int i = 0; i < n; i++) {
-            if(i - v[curr] > k) curr++;
-            if(curr == v.size()) break;
-            if(abs(i - v[curr]) <= k) ans.push_back(i);
+            while(curr < n && nums[curr] != key) curr++;
+            if(curr == nums.size()) break;
+            if(i - curr > k) {
+                curr++;
+                while(curr < n && nums[curr] != key) curr++;
+            }
+            if(curr == nums.size()) break;
+            if(abs(i - curr) <= k) ans.push_back(i);
         }
         return ans;
     }
