f012ce3e7a62349f8eb7b6c8884c1fce61ca25cd
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-24 12:06:07 +0530

    Time: 762 ms (19.13%) | Memory: 235.4 MB (6.42%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> fun(TreeNode* root, int& res) {
+        if(root == NULL) return {0, INT_MIN, INT_MAX};
+
+        auto left = fun(root -> left, res);
+        auto right = fun(root -> right, res);
+        if(root -> val > left[1] && root -> val < right[2]) {
+            int ans = left[0] + root -> val + right[0];
+            // ans = max(max(left[0], right[0]), ans);
+            // if(ans < 0) ans = 0;
+            res = max(res, ans);
+            return {ans, max(right[1], root -> val), min(left[2], root -> val)};
+        } else {
+            return {max(left[0], right[0]), INT_MAX, INT_MIN};
+        }
+    }
+    int maxSumBST(TreeNode* root) {
+        int res = 0;
+        fun(root, res);
+        return res;
+    }
+};
