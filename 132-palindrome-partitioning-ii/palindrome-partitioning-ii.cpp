bd799bccb409281114ba424c6014fa7c9f8b1cb4
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-24 14:06:19 +0530

    Time: 1797 ms (6.75%) | Memory: 9.5 MB (56.03%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     bool isPal(string& s, int i, int j) {
-        int n = s.size();
-        while(i < j) if(s[i++] != s[j--]) return false;
+        while(i < j) {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
         return true;
     }
-    int fun(string& s, int i, int n, vector<int>& dp) {
-        if(i == n) return 0;
+    int fun(int idx, string& s, vector<int>& dp) {
+        int n = s.size();
+        if(idx == n) return 1;
+        if(dp[idx] != -1) return dp[idx];
         int mn = INT_MAX;
-        if(dp[i] != -1) return dp[i];
-        for(int k = i; k < n; k++) {
-            int steps = INT_MAX;
-            if(isPal(s, i, k)) {
-                steps = 1 + fun(s, k + 1, n, dp);
-                mn = min(mn, steps);
+        for(int i = idx; i < n; i++) {
+            // string str = s.substr(idx, i - idx + 1);
+            // cout << str << endl;
+            if(isPal(s, idx, i)) {
+                mn = min(mn, 1 + fun(i + 1, s, dp));
             }
         }
-        return dp[i] = mn;
+        cout << mn << " ";
+        return dp[idx] = mn;
     }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n + 1, 0);
-        for(int i = n - 1; i >= 0; i--) {
-            int mn = INT_MAX;
-            for(int k = i; k < n; k++) {
-                int steps = INT_MAX;
-                if(isPal(s, i, k)) {
-                    steps = 1 + dp[k + 1];
-                    mn = min(mn, steps);
-                }
-            }
-            dp[i] = mn;
-        }
-        return dp[0] - 1;
+        vector<int> dp(n, -1);
+        return fun(0, s, dp) - 2;
     }
 };
