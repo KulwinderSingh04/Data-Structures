e1ea202c85f43554a4a33da02f64e0c7bfcbbb6d
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-26 15:04:55 +0530

    Time: 35 ms (5.28%) | Memory: 53.6 MB (5.02%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -11,19 +11,62 @@
  */
 class Solution {
 public:
-    bool fun(TreeNode* root1, TreeNode* root2) {
-        if(root1 == NULL && root2 == NULL) return true;
-        if(root1 == NULL || root2 == NULL) return false;
-        if(root1 -> val != root2 -> val) return false;
-        bool a = fun(root1 -> left, root2 -> left);
-        bool b = fun(root1 -> right, root2 -> right);
-        return a && b;
+    string fun(TreeNode* root) {
+        if(root == NULL) return "X";
+        string a = "";
+        a += ( '(' + to_string(root -> val) + ')');
+        string a1 = fun(root -> left);
+        string a2 = fun(root -> right);
+        a += '#' + a1 + '?' + a2;
+        return a;
+    }
+    void lps(string& pat, vector<int>& v) {
+        int n = pat.size();
+        int len = 0;
+        v[0] = 0;
+        int i = 1;
+        while(i < n) {
+            if(pat[i] == pat[len]) {
+                len++;
+                v[i] = len;
+                i++;
+            } else {
+                if(len == 0) {
+                    i++;
+                    v[i - 1] = 0;
+                }
+                else len = v[len - 1];
+            }
+        }
+    }
+    bool kmp(string& pat, string& txt) {
+        // code here
+        int n = txt.size();
+        vector<int> v(pat.size());
+        lps(pat, v);
+        int i = 0;
+        int j = 0;
+        vector<int> res;
+        while(i < txt.size()) {
+            if(txt[i] == pat[j]) {
+                i++;
+                j++;
+                if(j == pat.size()) {
+                    return true;
+                }
+            }
+            else {
+                if(j != 0) j = v[j - 1];
+                else i++;
+            }
+        }
+        return false;
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        if(root == NULL) return false;
-        if(root -> val == subRoot -> val && fun(root, subRoot)) return true;
-        bool a = isSubtree(root -> left, subRoot);
-        bool b = isSubtree(root -> right, subRoot);
-        return a || b;
+        string s1 = "", s2 = "";
+        s1 = fun(root);
+        s2 = fun(subRoot);
+        cout << s1 << " " << s2 << endl;
+        return kmp(s2, s1);
     }
 };
