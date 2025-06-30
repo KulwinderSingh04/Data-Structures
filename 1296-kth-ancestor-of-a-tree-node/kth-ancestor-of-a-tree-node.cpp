97cbb0f7836ccfee0a4097e9aa17208e7152b507
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-06-30 12:42:50 +0530

    Time: 32 ms (92.93%) | Memory: 116 MB (94.24%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.cpp
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.cpp
@@ -1,25 +1,19 @@
 class TreeAncestor {
 public:
     vector<vector<int>> dp;
-    void fun(vector<vector<int>>& dp, vector<int>& par) {
-        int n = par.size();
-        for(int i = 0; i < n; i++) {
-            dp[0][i] = par[i];
-        }
-
+    TreeAncestor(int n, vector<int>& parent) {
+        dp.resize(17, vector<int> (n));
+        dp[0] = parent;
         for(int i = 1; i < 17; i++) {
-            for(int j = 1; j < n; j++) {
-                if(dp[i - 1][j] != -1) dp[i][j] = dp[i - 1][dp[i - 1][j]];
+            for(int j = 0; j < n; j++) {
+                if(dp[i - 1][j] == -1) dp[i][j] = -1;
+                else dp[i][j] = dp[i - 1][dp[i - 1][j]];
             }
         }
     }
-    TreeAncestor(int n, vector<int>& parent) {
-        dp.resize(17, vector<int> (n, -1));
-        fun(dp, parent);
-    }
     
     int getKthAncestor(int node, int k) {
-        for(int i = 16; i >= 0; i--) {
+        for(int i = 0; i < 17; i++) {
             if((1 << i) & k) {
                 node = dp[i][node];
                 if(node == -1) return -1;
