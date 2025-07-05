848b770904e2f867902e2dfdcb94edbf1559a8f0
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-05 17:57:57 +0530

    Time: 47 ms (12.07%) | Memory: 57.6 MB (8.51%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    int find(int x, vector<int>& par) {
-        if(x == par[x]) return x;
-        return par[x] = find(par[x], par);
-    }
-    
-    void Union(int x, int y, vector<int>& par, vector<int>& rank) {
-        int x_par = find(x, par);
-        int y_par = find(y, par);
-        if(x_par == y_par) return;
-        if(rank[x_par] == rank[y_par]) {
-            par[x_par] = par[y_par];
-            rank[y_par]++;
-        }
-        else if(rank[x_par] > rank[y_par]) {
-            par[y_par] = par[x_par];
-        }
-        else {
-            par[x_par] = par[y_par];
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
+        vis[node] = 1;
+        for(auto x : adj[node]) {
+            if(vis[x] == 0) {
+                dfs(x, adj, vis);
+            }
         }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size() < n - 1) return -1;
-        vector<int> par(n);
-        for(int i = 0; i < n; i++) {
-            par[i] = i;
+        vector<vector<int>> adj(n);
+        for(auto edge : connections) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
-        vector<int> rank(n);
-        int cable = 0;
-        for(int i = 0; i < connections.size(); i++) {
-            if(find(connections[i][0], par) == find(connections[i][1], par)) cable++;
-            else {
-                Union(connections[i][0], connections[i][1], par, rank);
+        vector<int> vis(n);
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(vis[i] == 0) {
+                count++;
+                dfs(i, adj, vis);
             }
         }
-        int comp = 0;
-        for(int i = 0; i < n; i++) if(par[i] == i) comp++;
-        return comp - 1;
+        return count - 1;
     }
 };
