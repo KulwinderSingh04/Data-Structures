0ed2ad9f21b2898c2353146116f661d6a6234e05
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-05 17:42:59 +0530

    Time: 2 ms (71.71%) | Memory: 12.1 MB (64.91%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node == NULL) return NULL;
+        Node* root = new Node(node -> val);
+        queue<pair<Node*, Node*>> q;
+        q.push({root, node});
+        vector<Node*> nodeRegister(101, NULL); 
+        unordered_set<Node*> st;
+        nodeRegister[node -> val] = root;
+        st.insert(node);
+        while(q.size()) {
+            auto t = q.front();
+            q.pop();
+            auto r = t.first;
+            auto n = t.second;
+            for(auto x : n -> neighbors) {
+                
+                if(nodeRegister[x -> val]) {
+                    r -> neighbors.push_back(nodeRegister[x -> val]);
+                    
+                }
+                if(st.find(x) != st.end()) continue;
+                else {
+                    Node* newNode = new Node(x -> val);
+                    r -> neighbors.push_back(newNode);
+                    nodeRegister[x -> val] = newNode;
+                    q.push({newNode, x});
+                }
+                st.insert(x);
+            }
+        }
+        return root;
+        
+    }
+};
