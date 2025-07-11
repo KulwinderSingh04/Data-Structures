eb7d666108168ceeac43a5b0d31c13b703a80188
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-11 16:37:22 +0530

    Time: 2414 ms (5.02%) | Memory: 320.1 MB (5.36%) - LeetSync

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
+    int fun(int i, int mx, vector<vector<int>>& intervals, map<pair<int, int>, int>& dp) {
+        int n = intervals.size();
+        if(i == n) {
+            if(mx >= n - 1) return 0;
+            return 1e9;
+        }
+        if(mx < intervals[i][0]) return 1e9;
+        if(dp.find({i, mx}) != dp.end()) return dp[{i, mx}];
+        int p = 1 + fun(i + 1,max(mx, intervals[i][1]), intervals, dp);
+        int np = fun(i + 1, mx, intervals, dp);
+        return dp[{i, mx}] = min(p, np);
+    }
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> v(n + 1);
+        vector<vector<int>> intervals(n + 1);
         for(int i = 0; i < n + 1; i++) {
             int lo = max(0, i - ranges[i]);
             int hi = min(n, i + ranges[i]);
-            v[lo] = max(v[lo], hi);   
+            intervals[i] = {lo, hi};
         }
-        int tap = 0;
-        int mxEnd = 0, currEnd = 0;
-        for(int i = 0; i < n + 1; i++) {
-            if(i > mxEnd) return -1;
-            if(i > currEnd) {
-                tap++;
-                currEnd = mxEnd; 
-            }
-            mxEnd = max(mxEnd, v[i]);
-        } 
-        return tap;
+        sort(intervals.begin(), intervals.end());
+        map<pair<int, int>, int> dp;
+        int t = fun(0, 0, intervals, dp);
+        return t == 1e9 ? -1 : t;
     }
 };
