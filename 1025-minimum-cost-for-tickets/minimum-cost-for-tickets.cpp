c2cbf72cccc292e4ab3f5c022459958c8d91002c
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-22 10:09:30 +0530

    Time: 1 ms (35.42%) | Memory: 13.3 MB (50.23%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int fun(int i, vector<int>& days, vector<int>& costs, vector<int>& dp) {
-        int n = days.size();
-        if(i >= n) return 0;
-        if(dp[i] != -1) return dp[i];
-        int res = INT_MAX;
-        for(int j = 0; j < 3; j++) {
-            int idx;
-            if(j == 0) idx = lower_bound(days.begin() + i, days.end(), days[i] + 1) - days.begin();
-            else if(j == 1) idx = lower_bound(days.begin() + i, days.end(), days[i] + 7) - days.begin();
-            else  idx = lower_bound(days.begin() + i, days.end(), days[i] + 30) - days.begin();
-            // cout << idx << " ";
-            res = min(res, costs[j] + fun(idx, days, costs, dp));
-        }
-        return dp[i] = res;
-    }
+    // int fun(int i, vector<int>& days, vector<int>& costs, vector<int>& dp) {
+    //     int n = days.size();
+    //     if(i >= n) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     int res = INT_MAX;
+    //     for(int j = 0; j < 3; j++) {
+    //         int idx;
+    //         if(j == 0) idx = lower_bound(days.begin() + i, days.end(), days[i] + 1) - days.begin();
+    //         else if(j == 1) idx = lower_bound(days.begin() + i, days.end(), days[i] + 7) - days.begin();
+    //         else  idx = lower_bound(days.begin() + i, days.end(), days[i] + 30) - days.begin();
+    //         res = min(res, costs[j] + fun(idx, days, costs, dp));
+    //     }
+    //     return dp[i] = res;
+    // }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp(n, -1);
-        return fun(0, days, costs, dp);
+        vector<int> dp(n + 1);
+        dp[n] = 0;
+        for(int i = n - 1; i>= 0; i--) {
+            int res = INT_MAX;
+            for(int j = 0; j < 3; j++) {
+                int idx;
+                if(j == 0) idx = lower_bound(days.begin() + i, days.end(), days[i] + 1) - days.begin();
+                else if(j == 1) idx = lower_bound(days.begin() + i, days.end(), days[i] + 7) - days.begin();
+                else  idx = lower_bound(days.begin() + i, days.end(), days[i] + 30) - days.begin();
+                res = min(res, costs[j] + dp[idx]);
+            }
+            dp[i] = res;
+        }
+        return dp[0];
     }
 };
