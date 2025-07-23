4e0bfef5e0dd00833e7ace59f71038a708bceab5
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-23 16:23:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (89.19%) - LeetSync

---
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        int i = 3;
+        int j = 3;
+        while(j < n) {
+            if(i - 3 >= 0 && s[i - 1] == 'c' && s[i - 2] == 'b' && s[i - 3] == 'a') {
+                i -= 3;
+            }
+            s[i] = s[j];
+            j++;
+            i++;
+        }
+        cout << i << " ";
+        if(i == 0 || s.substr(0, i) == "abc") return true;
+        return false;
+    }
+};
