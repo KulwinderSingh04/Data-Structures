b497a99db1e03ac731f35cb119cc080e83b02942
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-27 09:26:34 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (77.89%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     int countHillValley(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
-        vector<int> v;
-        v.push_back(nums[0]);
-        for(int i = 1; i < n; i++) {
-            if(nums[i - 1] != nums[i]) v.push_back(nums[i]);
-        }
-        for(int i = 1; i < v.size() - 1; i++) {
-            if((v[i - 1] > v[i] && v[i + 1] > v[i]) || v[i - 1] < v[i] && v[i + 1] < v[i]) {
+        int i = 1;
+        int l = 0;
+        int ans=  0;
+        while(nums[i] == nums[0]) i++;
+        int r = i;
+        while(r < n && nums[r] == nums[i]) r++;
+        if(r == n) return 0;
+        if(i == n) return 0;
+        while(i < n) {
+            if((nums[l] > nums[i] && nums[r] > nums[i]) || (nums[l] < nums[i] && nums[r] < nums[i])) {
                 ans++;
             }
+            l = i;
+            i = r;
+            while(r < n && nums[i] == nums[r]) r++;
+            if(r == n) return ans;
         }
-        return ans;
+        return 0;
     }
 };
