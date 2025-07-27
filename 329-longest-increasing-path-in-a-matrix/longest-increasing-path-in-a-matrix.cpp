cefaa7d79a923613499a21fb1aba6b8b96d1e0ec
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-27 15:42:37 +0530

    Time: 13 ms (54.57%) | Memory: 21.8 MB (47.23%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int MOD = 1e9+7;
+    vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    bool isSafe(int r, int c, vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        return r >= 0 && r < n && c >= 0 && c < m;
+    }
+    int solve(int r, int c, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        int ans = 0;
+        if(dp[r][c] != -1) return dp[r][c];
+        for(int i = 0; i < 4; i++) {
+            int nr = r + dir[i][0];
+            int nc = c + dir[i][1];
+            if(isSafe(nr, nc, grid) && grid[nr][nc] > grid[r][c]) {
+                ans = max(ans, solve(nr, nc, grid, dp));
+            }
+        }
+        return dp[r][c] = 1 + ans;
+    }
+    int longestIncreasingPath(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int res = 0;
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                res = max(res, solve(i, j, grid, dp));
+            }
+        }
+        return res;
+    }
+};
