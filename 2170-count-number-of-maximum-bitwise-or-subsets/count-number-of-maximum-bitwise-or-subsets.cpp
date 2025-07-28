aeb26bfa0ece4ec5a19470a85e2bb1c73d805f12
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-07-28 09:49:43 +0530

    Time: 408 ms (10.63%) | Memory: 22.5 MB (21.26%) - LeetSync

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fun(int i, int bitOr, int& mx, vector<int>& nums, unordered_map<string, int>& dp) {
+        int n = nums.size();
+        if(i == n) return bitOr == mx;
+        string str = to_string(i) + '_' + to_string(bitOr);
+        // if(dp.find(Str) != dp.end()) return dp[str];
+        int p = fun(i + 1, bitOr | nums[i], mx,  nums, dp);
+        int np = fun(i + 1, bitOr, mx, nums, dp);
+        return dp[str] = np + p; 
+    }
+    int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
+
+        int mx = 0;
+        for(auto x : nums) mx |= x;
+        unordered_map<string, int> dp;
+        return fun(0, 0, mx, nums, dp);
+    }
+};
