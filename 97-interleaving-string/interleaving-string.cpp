39e0d59a2de20300ff220f7acfd3b9f6bc0ce7df
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-11 17:33:47 +0530

    Time: 3 ms (61.85%) | Memory: 11.2 MB (30.74%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    bool fun(int i, int j, int k, string& s1, string& s2, string& s3, vector<vector<vector<int>>>& dp) {
+    bool fun(int i, int j, string& s1, string& s2, string& s3, vector<vector<int>>& dp) {
+        int k = i + j;
         if(j == s2.size()) {
             return s3.substr(k) == s1.substr(i);
         }
         if(i == s1.size()) {
             return s3.substr(k) == s2.substr(j);
         }
-        if(dp[i][j][k] != -1) return dp[i][j][k];
+        if(dp[i][j] != -1) return dp[i][j];
         bool a = false, b = false;
-        if(s1[i] == s3[k]) a = fun(i + 1, j, k + 1, s1, s2, s3, dp);
-        if(s2[j] == s3[k]) b = fun(i, j + 1, k + 1, s1, s2, s3, dp);
-        return dp[i][j][k] = a || b;
+        if(s1[i] == s3[k]) a = fun(i + 1, j, s1, s2, s3, dp);
+        if(s2[j] == s3[k]) b = fun(i, j + 1, s1, s2, s3, dp);
+        return dp[i][j] = a || b;
     }
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size()) return false;
-        vector<vector<vector<int>>> dp(s1.size(), vector<vector<int>> (s2.size(), vector<int> (s3.size(), -1)));
-        return fun(0, 0, 0, s1, s2, s3, dp);
+        vector<vector<int>> dp(s1.size(), vector<int> (s2.size(), -1));
+        return fun(0, 0, s1, s2, s3, dp);
     }
 };
