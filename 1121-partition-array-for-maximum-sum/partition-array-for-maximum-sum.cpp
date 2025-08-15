bec36a2476bc041530ced6ad2273175381b7c614
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-15 22:57:50 +0530

    Time: 9 ms (29.23%) | Memory: 12.4 MB (62.25%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    int fun(vector<int>& arr, int i, int k, vector<int>& dp) {
+    int fun(int i, vector<int>& arr, int k, vector<int>& dp) {
         int n = arr.size();
-        if(i >= n) return 0;
+        if(i == n) return 0;
         if(dp[i] != -1) return dp[i];
-        int ans = 0;
         int mx = 0;
-        for(int t = i; t < min(n, i + k); t++) {
-            mx = max(mx, arr[t]);
-            int step = mx * (t - i + 1) + fun(arr, t + 1, k, dp);
-            ans = max(step, ans);
+        int res = 0;
+        for(int j = i; j < min(i + k, n); j++) {
+            mx = max(mx, arr[j]);
+            res = max(res, mx * (j - i + 1) + fun(j + 1, arr, k, dp));
         }
-        return dp[i] = ans;
+        return dp[i] = res;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n + 1, 0);
-        for(int i = n - 1; i >= 0; i--) {
-            int ans = 0;
-            int mx = 0;
-            for(int t = i; t < min(n, i + k); t++) {
-                mx = max(mx, arr[t]);
-                int step = mx * (t - i + 1) + dp[t + 1];
-                ans = max(step, ans);
-            }
-            dp[i] = ans;
-        }
-        return dp[0];
+        vector<int> dp(n, -1);
+        return fun(0, arr, k, dp);
     }
 };
