d5a164f687b5eb7674cccedc04b4a45280a1e3a4
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-16 21:06:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.22%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string str = to_string(num);
+        int n = str.size();
+        for(int i = 0; i < n; i++) {
+            if(str[i] == '6') {
+                str[i] = '9';
+                return stoi(str);
+            }
+        }
+        return num;
+    }
+};
