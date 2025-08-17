8b4885309a83b1e988c3aaf984de55029cb1eb80
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-17 13:01:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (21.60%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int mn = 0;
-        int mx = 0;
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '(') {
-                mn++;
-                mx++;
-            } else if(s[i] == ')') {
-                mn--;
-                mx--;
+        stack<int> br, ast;
+        int n = s.size();
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(') br.push(i);
+            else if(s[i] == ')') {
+                if(br.size()) br.pop();
+                else {
+                    if(ast.size() == 0) return false;
+                    else {
+                        if(ast.top() < i) ast.pop();
+                        else return false;
+                    }
+                }
             } else {
-                mn--;
-                mx++;
+                ast.push(i);
             }
-            if(mn < 0) mn = 0;
-            if(mx < 0) return false;
         }
-        return mn == 0;
+        if(br.size() > ast.size()) return false;
+        while(br.size()) {
+            if(br.top() > ast.top()) return false;
+            ast.pop();
+            br.pop();
+        }
+        return true;
     }
 };
