a97ba511c8d369f74f6fdee97683375d1bf9e4a4
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-20 09:10:39 +0530

    Time: 13 ms (18.92%) | Memory: 30.4 MB (21.64%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -3,26 +3,20 @@ public:
     int countSquares(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> grid(n, vector<int> (m, 0));
-        int sum = 0;
-        for(int i = 0; i < n; i++) {
-            grid[i][0] = matrix[i][0];
-            sum += grid[i][0];
-        }
-        for(int i = 1; i < m; i++) {
-            grid[0][i] = matrix[0][i];
-            sum += grid[0][i];
-        }
-        for(int i = 1; i < n; i++) {
-            for(int j = 1; j < m; j++) {
+        vector<vector<int>> ans(n, vector<int> (m, 0));
+        int res = 0;
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
                 if(matrix[i][j] == 0) continue;
-                int mn = min(grid[i][j - 1], min(grid[i - 1][j - 1], grid[i - 1][j]));
-                grid[i][j] = 1 + mn;
-                // cout << grid[i][j] << " ";
-                sum += grid[i][j];
+                int a = 0, b = 0, c = 0;
+                if(i + 1 < n) a = ans[i + 1][j];
+                if(j + 1 < m) b = ans[i][j + 1];
+                if(i + 1 < n && j + 1 < m) c = ans[i + 1][j + 1];
+                ans[i][j] = min({a, b , c}) + 1;
+                res += ans[i][j];
             }
-            // cout << endl;
+            cout << endl;
         }
-        return sum;
+        return res;
     }
 };
