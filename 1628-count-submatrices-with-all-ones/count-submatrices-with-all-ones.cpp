f2fcb4616198e0924d9a0292c059ca5be865a179
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-21 11:44:28 +0530

    Time: 11 ms (57.45%) | Memory: 17.9 MB (82.59%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -3,24 +3,22 @@ public:
     int numSubmat(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (m, vector<int> (3)));
-
         int ans = 0;
+        for(int i = 0; i < n; i++) {
+            int c = 0;
+            for(int j = m - 1; j >= 0; j--) {
+                if(mat[i][j] == 1) c++;
+                else c = 0;
+                mat[i][j] = c;
+            }
+        }
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(mat[i][j] == 0) continue;
-                int n1 = n;
-                int m1 = m;
-                for(int k1 = i; k1 < n1; k1++) {
-                    for(int k2 = j; k2 < m1; k2++) {
-                        if(mat[k1][k2] == 1) {
-                            // cout << k1 << " " << k2 << endl;
-                            ans++;
-                        } else {
-                            m1 = min(m1, k2);
-                        }
-                    }
-                    if(mat[k1][j] == 0) n1 = min(n1, k1);
+                int mn = 500;
+                for(int k = i; k < n; k++) {
+                    mn = min(mn, mat[k][j]);
+                    ans += mn;
                 }
             }
         }
