5085aedf41e3e02a8662b616472b43fa3570aafe
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-22 10:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (59.70%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int fun(int copy, int note, int flag, int n, vector<vector<int>>& dp) {
-        if(note == n) return 0;
-        if(note > n) return 1e8;
-        if(dp[copy][note] != -1) return dp[copy][note];
-        int a = 1e8;
-        if(flag == 0) a = 1 + fun(note, note, 1, n, dp);
-        int b = 1e8;
-        if(copy != 0) b = 1 + fun(copy, note + copy, 0, n, dp);
-        return dp[copy][note] = min(a, b);
-    }
     int minSteps(int n) {
-        vector<vector<int>> dp(n + 1, vector<int> (n + 1, -1));
-        return fun(0, 1, 0, n, dp);
+        int ans = 0;
+        while(n > 1) {
+            int highFactor = 1;
+            for(int i = n / 2; i > 0; i--) {
+                if(n % i == 0) {
+                    highFactor = i;
+                    break;
+                }
+            }
+            ans += n / highFactor;
+            n = highFactor;
+        }
+        return ans;
     }
 };
