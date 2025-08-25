52b8cdbecd1b43c2226c960b9a6dcab196efeb11
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-08-25 16:58:30 +0530

    Time: 56 ms (89.05%) | Memory: 109.7 MB (24.09%) - LeetSync

--- 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.cpp
+++ 3485-maximize-score-of-numbers-in-ranges/maximize-score-of-numbers-in-ranges.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    bool fun(int mid, vector<int>& start, int d) {
-        long long a = start[0];
-        for(int i = 1; i < start.size(); i++) {
-            long long b = max(a + mid, 1LL * start[i]);
-            if(b >= start[i] && b <= start[i] + d) {
-                a = b;
-            } else return false;
+    bool fun(int mid,vector<int>& start, int d) {
+        long long a=start[0];
+        for(int i=1;i<start.size();i++) {
+            if(a+mid>start[i]+d) return false;
+            if(a + mid>=start[i]) a+=mid;
+            else a=start[i];
         }
         return true;
     }
     int maxPossibleScore(vector<int>& start, int d) {
-        int n = start.size();
-        sort(start.begin(), start.end());
-        int lo = 0;
-        int hi = start.back() + d;
-        int ans = 0;
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(fun(mid, start, d)) {
-                ans = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
+        // vector<int> v;
+        sort(start.begin(),start.end());
+        long long n=start.size();
+        long long lo=0;
+        long long hi=start[n-1]+d;
+        long long ans=0;
+        while(lo<=hi) {
+            int mid=lo+(hi-lo)/2;
+            if(fun(mid,start,d)) {
+                ans=mid;
+                lo=mid+1;
             }
+            else hi=mid-1;
         }
         return ans;
     }
