ebcfa9063802a8c3d5bbc4bd46f8be6bf4cee703
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-09-04 15:25:08 +0530

    Time: 170 ms (75.64%) | Memory: 148.4 MB (82.60%) - LeetSync

--- 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1643-number-of-nodes-in-the-sub-tree-with-the-same-label/number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void fun(int node, int par, vector<vector<int>>& adj, unordered_map<int, char>& mp, vector<int>& v, vector<int>& ans) {
+    void fun(int node, int par, vector<vector<int>>& adj, string& labels, vector<int>& v, vector<int>& ans) {
         int n = adj.size();
-        int before = v[mp[node] - 97];
+        int before = v[labels[node] - 97];
         for(auto x : adj[node]) {
             if(x != par) {
-                fun(x, node, adj, mp, v, ans);
+                fun(x, node, adj, labels, v, ans);
             }
         }
-        v[mp[node] - 97]++;
-        int after = v[mp[node] - 97];
+        v[labels[node] - 97]++;
+        int after = v[labels[node] - 97];
         ans[node] = after - before;
     }
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-        unordered_map<int, char> mp;
-        for(int i = 0; i < n; i++) mp[i] = labels[i];
         vector<vector<int>> adj(n);
         for(auto x : edges) {
             adj[x[0]].push_back(x[1]);
             adj[x[1]].push_back(x[0]);
         }
         vector<int> ans(n), v(26);
-        fun(0, -1, adj, mp, v, ans);
+        fun(0, -1, adj, labels, v, ans);
         return ans;
     }
 };
