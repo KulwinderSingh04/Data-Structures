ca4e39e9635576adbff33c690bcda97ef02eb4da
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-09-18 20:29:28 +0530

    Time: 72 ms (38.87%) | Memory: 24.4 MB (41.36%) - LeetSync

---
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    double distCalc(int x1, int y1, int x2, int y2) {
+        return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    }
+    int dfs(int node, vector<vector<int>>& adj, vector<int>& vis) {
+        vis[node] = 1;
+        int ans = 1;
+        for(auto x : adj[node]) {
+            if(vis[x] == 0) {
+                ans += dfs(x, adj, vis);
+            }
+        }
+        return ans;
+    }
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+        vector<vector<int>> adj(n);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                double dis = distCalc(bombs[i][0], bombs[i][1], bombs[j][0], bombs[j][1]);
+                if(dis <= bombs[i][2]) {
+                    adj[i].push_back(j);
+                }
+                if(dis <= bombs[j][2]) {
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            vector<int> vis(n);
+            ans = max(ans, dfs(i, adj, vis));
+        }
+        return ans;
+    }
+};
