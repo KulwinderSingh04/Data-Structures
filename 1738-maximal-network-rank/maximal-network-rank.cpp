2984c5374b8ba81197e70478a4ea190d138864c6
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-09-19 15:12:44 +0530

    Time: 187 ms (5.09%) | Memory: 69 MB (5.09%) - LeetSync

---
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        set<vector<int>> st(roads.begin(), roads.end());
+        unordered_map<int, int> degree;
+        for(auto x : roads) {
+            degree[x[0]]++;
+            degree[x[1]]++;
+        }
+        vector<pair<int, int>> a;
+        for(auto x : degree) a.push_back(x);
+        // sort(a.begin(), a.end(), [](pair<int, int>& x, pair<int, int>& y) {
+        //     return x.second > y.second;
+        // });
+        // for(int i = 0; i < a.size(); i++) cout << a[i].first << " " << a[i].second << endl;
+        if(a.size() == 0) return 0;
+        int ans = 0;
+        for(int i = 0; i < a.size(); i++) {
+            for(int j = 0; j < a.size(); j++) {
+                if(i == j) continue;
+                int x = a[i].second + a[j].second;
+                if(st.find({a[i].first, a[j].first}) != st.end() || st.find({a[j].first, a[i].first}) != st.end()) x--;
+                ans = max(ans, x);
+            }
+        }
+        return ans;
+
+    }
+};
