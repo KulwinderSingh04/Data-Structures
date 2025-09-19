af145eb363c8d36fe8fa985df2a26b92bba0d3f1
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-09-19 15:13:29 +0530

    Time: 83 ms (7.55%) | Memory: 47.6 MB (8.55%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -9,9 +9,9 @@ public:
         }
         vector<pair<int, int>> a;
         for(auto x : degree) a.push_back(x);
-        // sort(a.begin(), a.end(), [](pair<int, int>& x, pair<int, int>& y) {
-        //     return x.second > y.second;
-        // });
+        sort(a.begin(), a.end(), [](pair<int, int>& x, pair<int, int>& y) {
+            return x.second > y.second;
+        });
         // for(int i = 0; i < a.size(); i++) cout << a[i].first << " " << a[i].second << endl;
         if(a.size() == 0) return 0;
         int ans = 0;
@@ -20,7 +20,8 @@ public:
                 if(i == j) continue;
                 int x = a[i].second + a[j].second;
                 if(st.find({a[i].first, a[j].first}) != st.end() || st.find({a[j].first, a[i].first}) != st.end()) x--;
-                ans = max(ans, x);
+                if(x >= ans) ans = x;
+                else break;
             }
         }
         return ans;
