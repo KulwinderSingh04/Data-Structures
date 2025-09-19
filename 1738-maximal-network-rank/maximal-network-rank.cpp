1e47107b92fd04a804a465c782d1364c88ec68e2
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-09-19 15:21:34 +0530

    Time: 8 ms (77.89%) | Memory: 37.7 MB (59.71%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.cpp
+++ 1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        set<vector<int>> st(roads.begin(), roads.end());
-        unordered_map<int, int> degree;
+        if(roads.size() == 0) return 0;
+        vector<vector<int>> adj(n, vector<int> (n));
+        vector<int> degree(n);
         for(auto x : roads) {
             degree[x[0]]++;
             degree[x[1]]++;
+            adj[x[0]][x[1]] = 1;
+            adj[x[1]][x[0]] = 1;
         }
-        vector<pair<int, int>> a;
-        for(auto x : degree) a.push_back(x);
-        sort(a.begin(), a.end(), [](pair<int, int>& x, pair<int, int>& y) {
-            return x.second > y.second;
-        });
-        // for(int i = 0; i < a.size(); i++) cout << a[i].first << " " << a[i].second << endl;
-        if(a.size() == 0) return 0;
         int ans = 0;
-        for(int i = 0; i < a.size(); i++) {
-            for(int j = 0; j < a.size(); j++) {
-                if(i == j) continue;
-                int x = a[i].second + a[j].second;
-                if(st.find({a[i].first, a[j].first}) != st.end() || st.find({a[j].first, a[i].first}) != st.end()) x--;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int x = degree[i]+ degree[j];
+                if(adj[i][j] == 1) x--; 
                 if(x >= ans) ans = x;
-                else break;
             }
         }
         return ans;
