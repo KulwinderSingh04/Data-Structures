62cde15169dbcfaf0f63ba6e69af8c4bcaf6d1cf
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-09-25 11:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (42.12%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int fun(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-        int n = triangle.size();
-        if(i == n) return 0;
-        if(dp[i][j] != -1e6) return dp[i][j];
-        int a = triangle[i][j] + fun(i + 1, j, triangle, dp);
-        int b = triangle[i][j] + fun(i + 1, j + 1, triangle, dp);
-        return dp[i][j] = min(a, b);
-    }
+    // int fun(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+    //     int n = triangle.size();
+    //     if(i == n) return 0;
+    //     if(dp[i][j] != -1e6) return dp[i][j];
+    //     int a = triangle[i][j] + fun(i + 1, j, triangle, dp);
+    //     int b = triangle[i][j] + fun(i + 1, j + 1, triangle, dp);
+    //     return dp[i][j] = min(a, b);
+    // }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int> (n));
@@ -16,6 +16,6 @@ public:
                 dp[i][j] = triangle[i][j] + (i != n - 1 ? min(dp[i + 1][j + 1], dp[i + 1][j]) : 0);
             }
         }
-        return fun(0, 0, triangle, dp);
+        return dp[0][0];
     }
 };
