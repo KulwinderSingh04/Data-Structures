e09224bf3fb3b6aaca8674436b38c76516c07ffe
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-02 13:54:11 +0530

    Time: 101 ms (41.65%) | Memory: 71.4 MB (71.38%) - LeetSync

--- 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
@@ -1,38 +1,41 @@
 class Graph {
 public:
     typedef pair<int, int> pp;
-    vector<vector<pp>> adj;
+    vector<vector<int>> adj;
     Graph(int n, vector<vector<int>>& edges) {
-        adj.resize(n);
+        adj.resize(n, vector<int> (n, INT_MAX));
+        for(int i = 0; i < n; i++) adj[i][i] = 0;
         for(auto x : edges) {
-            adj[x[0]].push_back({x[1], x[2]});
+            adj[x[0]][x[1]] = x[2];
+        }
+        for(int k = 0; k < n; k++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if( adj[i][k] != INT_MAX && adj[k][j] != INT_MAX && adj[i][j] > adj[i][k] + adj[k][j]) {
+                        adj[i][j] = adj[i][k] + adj[k][j];
+                    }
+                }
+            }
         }
     }
     
     void addEdge(vector<int> x) {
-        adj[x[0]].push_back({x[1], x[2]});
-    }
-    
-    int shortestPath(int node1, int node2) {
-        priority_queue<pp, vector<pp>, greater<pp>> pq;
-        pq.push({0, node1});
         int n = adj.size();
-        vector<int> dist(n, INT_MAX);
-        dist[node1] = 0;
-        while(pq.size()) {
-            auto t = pq.top();
-            pq.pop();
-            int dis = t.first;
-            int node = t.second;
-            for(auto x : adj[node]) {
-                if(dist[x.first] > dis + x.second) {
-                    dist[x.first] = dis + x.second;
-                    pq.push({dist[x.first], x.first});
+        if(adj[x[0]][x[1]] > x[2]) {
+            adj[x[0]][x[1]] = x[2];
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(adj[i][x[0]] != INT_MAX && adj[x[1]][j] != INT_MAX && adj[i][j] > adj[i][x[0]] + x[2] + adj[x[1]][j]) {
+                        adj[i][j] = adj[i][x[0]] + x[2] + adj[x[1]][j];
+                    }
                 }
             }
         }
-        if(dist[node2] == INT_MAX) return -1;
-        return dist[node2];
+
+    }
+    
+    int shortestPath(int node1, int node2) {
+        return adj[node1][node2] == INT_MAX ? -1 : adj[node1][node2];
     }
 };
 
