6d3e51bfe41f1b0c34551149ad84d30b04f02473
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-02 12:01:57 +0530

    Time: 146 ms (5.02%) | Memory: 41.1 MB (39.25%) - LeetSync

---
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int find(int x, vector<int>& par) {
+        if(x == par[x]) return x;
+        return par[x] = find(par[x], par);
+    }
+    void Union(int x, int y, vector<int>& par, vector<int>& rank) {
+        x = find(x, par);
+        y = find(y, par);
+        if(rank[x] == rank[y]) {
+            rank[x]++;
+            par[y] = x;
+        } else if(rank[x] > rank[y]) {
+            par[y] = x;
+        } else par[x] = y;
+    }
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int> par(n), rank(n);
+        for(int i = 0; i < n; i++) par[i] = i;
+        unordered_set<int> parSet;
+        for(int i = 0; i < n; i++) {
+            cout << i << " ";
+            if(parSet.find(leftChild[i]) != parSet.end() || parSet.find(rightChild[i]) != parSet.end()) return false;
+            if(leftChild[i] != -1) {
+                if(find(i, par) != find(leftChild[i], par)) {
+                    parSet.insert(leftChild[i]);
+                    Union(i, leftChild[i], par, rank);
+                } else return false;
+            }
+            if(rightChild[i] != -1) {
+                if(find(i, par) != find(rightChild[i], par)) {
+                    parSet.insert(rightChild[i]);
+                    Union(i, rightChild[i], par, rank);
+                } else return false;
+            }
+        }
+        int count = 0;
+
+        
+        for(int i = 0; i < n; i++) if(par[i] == i) count++;
+        cout << count;
+        return count == 1;
+    }
+};
