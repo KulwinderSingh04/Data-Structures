1c835a9ec769028f3aac32b81f225abcc30ef30f
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-03 14:27:58 +0530

    Time: 236 ms (24.16%) | Memory: 83 MB (17.11%) - LeetSync

--- 3217-number-of-possible-sets-of-closing-branches/number-of-possible-sets-of-closing-branches.cpp
+++ 3217-number-of-possible-sets-of-closing-branches/number-of-possible-sets-of-closing-branches.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     typedef pair<int, int> pp;
     int numberOfSets(int n, int maxDistance, vector<vector<int>>& roads) {
-        sort(roads.begin(), roads.end(), [](vector<int>& a, vector<int>& b) {
-            return a[2] < b[2];
-        });
         int ans = 0;
         for(int gph = 0; gph < (1 << n); gph++) {
             vector<vector<int>> dist(n, vector<int> (n, INT_MAX));
@@ -12,13 +9,9 @@ public:
             for(auto road : roads) {
                 int u = road[0];
                 int v = road[1];
-                if(st.find({u, v}) != st.end()) continue;
-                cout << (gph & (1 << u)) <<" " << (gph & (1 << v)) << endl;
                 if((gph & (1 << u)) && (gph & (1 << v))) {
-                    dist[u][v] = road[2];
-                    dist[v][u] = road[2];
-                    st.insert({u, v});
-                    st.insert({v, u});
+                    dist[u][v] = min(dist[u][v], road[2]);
+                    dist[v][u] = min(dist[v][u], road[2]);
                 } 
             }
             for(int i = 0; i < n; i++) dist[i][i] = 0;
