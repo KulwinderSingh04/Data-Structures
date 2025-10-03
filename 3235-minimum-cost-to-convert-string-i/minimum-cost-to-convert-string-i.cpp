58c35530efbe17c20eec622964094d55a3b3aa76
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-03 15:15:25 +0530

    Time: 25 ms (85.56%) | Memory: 100.4 MB (36.73%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-    typedef pair<char, long long> pp1;
-    typedef pair<long long, char> pp2;
-    long long bfs(char src, char tar, unordered_map<char, vector<pp1>>& adj) {
-        vector<long long> dist(26, LLONG_MAX);
-        priority_queue<pp2, vector<pp2>, greater<pp2>> pq;
-        pq.push({0, src});
-        dist[src - 97] = 0;
-        while(pq.size()) {
-            auto t = pq.top();
-            pq.pop();
-            for(auto x : adj[t.second]) {
-                if(dist[x.first - 97] > t.first + x.second) {
-                    dist[x.first - 97] = t.first + x.second;
-                    pq.push({dist[x.first - 97], x.first});
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<long long>> dist(26, vector<long long> (26, LLONG_MAX));
+        for(int i = 0; i < changed.size(); i++) {
+            dist[original[i] - 'a'][changed[i] - 'a'] = min(dist[original[i] - 'a'][changed[i] - 'a'], (long long)cost[i]); 
+        }
+        for(int i = 0; i < 26; i++) dist[i][i] = 0;
+        
+        for(int k = 0; k < 26; k++) {
+            for(int i = 0; i < 26; i++) {
+                for(int j = 0; j < 26; j++) {
+                    if(dist[i][k] != LLONG_MAX && dist[k][j] != LLONG_MAX && dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
                 }
             }
         }
-        if(dist[tar - 97] == LLONG_MAX) return -1;
-        return dist[tar - 97];
-
-    }
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        unordered_map<char, vector<pp1>> adj;
-        for(int i = 0; i < changed.size(); i++) {
-            adj[original[i]].push_back({changed[i], cost[i]});
-        }
         long long ans = 0;
-        map<pair<char, char>, long long> mp;
         for(int i = 0; i < source.size(); i++) {
             if(source[i] == target[i]) continue;
-            if(mp.find({source[i], target[i]}) != mp.end()) {
-                ans += mp[{source[i], target[i]}];
-                continue;
-            }
-            long long t = bfs(source[i], target[i], adj);
-            if(t == -1) return t;
-            mp[{source[i], target[i]}] = t;
-            ans += t;
+            if(dist[source[i] - 'a'][target[i] - 'a'] == LLONG_MAX) return -1;
+            ans += dist[source[i] - 'a'][target[i] - 'a'];
         }
         return ans;
     }
