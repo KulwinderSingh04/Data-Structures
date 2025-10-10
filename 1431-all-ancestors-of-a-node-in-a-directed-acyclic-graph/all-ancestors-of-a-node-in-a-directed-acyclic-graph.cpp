56f51fa75712796bd2719eb99078b7aee535c336
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-10 22:42:39 +0530

    Time: 343 ms (39.97%) | Memory: 157.8 MB (18.89%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    void dfs(int node, int par, vector<vector<int>>& adj, vector<vector<int>>& ans) {
-        // if(node != par &&  (ans[node].size() == 0 || ans[node].back() != par)) ans[node].push_back(par);
+    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis, stack<int>& st) {
+        vis[node] = 1;
         for(auto& x : adj[node]) {
-            if(ans[x].size() == 0 || ans[x].back() != par) {
-                ans[x].push_back(par);
-                dfs(x, par, adj, ans);
-            }
-            
+            if(vis[x] == 0) dfs(x, adj, vis, st);
         }
+        st.push(node);
     }
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         // cout << edges.size();
         vector<vector<int>> adj(n);
+        vector<int> indegree(n);
         for(auto& x : edges) {
             adj[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
+        }
+        vector<int> vis(n);
+        stack<int> st;
+        for(int i = 0; i < n; i++) {
+            if(vis[i] == 0) {
+                dfs(i, adj, vis, st);
+            }
+        }
+        vector<int> topoSort;
+        while(st.size()) {
+            topoSort.push_back(st.top());
+            st.pop();
+        }
+        vector<set<int>> ancestor(n);
+        for(int i = 0; i < n; i++) {
+            for(auto x : adj[topoSort[i]]) {
+                ancestor[x].insert(topoSort[i]);
+                for(auto t : ancestor[topoSort[i]]) {
+                    ancestor[x].insert(t);
+                }
+            }
         }
         vector<vector<int>> ans(n);
         for(int i = 0; i < n; i++) {
-            dfs(i, i, adj, ans);
+            for(auto x : ancestor[i]) {
+                ans[i].push_back(x);
+            }
         }
         return ans;
     }
