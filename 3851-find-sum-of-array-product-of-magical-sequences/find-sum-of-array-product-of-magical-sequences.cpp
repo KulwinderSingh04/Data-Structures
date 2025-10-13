e918984fed6b138d2f6ef251c40d0d4ce6033efc
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-13 12:41:59 +0530

    Time: 1506 ms (7.14%) | Memory: 74.2 MB (28.57%) - LeetSync

--- 3851-find-sum-of-array-product-of-magical-sequences/find-sum-of-array-product-of-magical-sequences.cpp
+++ 3851-find-sum-of-array-product-of-magical-sequences/find-sum-of-array-product-of-magical-sequences.cpp
@@ -1,70 +1,49 @@
 class Solution {
 public:
-    long long quickmul(long long x, long long y, long long mod) {
-        long long res = 1, cur = x % mod;
-        while (y) {
-            if (y & 1) {
-                res = res * cur % mod;
-            }
-            y >>= 1;
-            cur = cur * cur % mod;
-        }
-        return res;
-    };
-
-    int magicalSum(int m, int k, vector<int>& nums) {
+    int MOD = 1e9 + 7;
+    vector<long long> fact, invFact;
+    long long pow(long long a, long long b) {
+        if(b == 0) return 1;
+        long long x = pow(a, b / 2);
+        if(b % 2) return (a * ((x * x) % MOD)) % MOD;
+        return (x * x) % MOD;
+    }
+    long long nCrMod(int n, int r) {
+        if(r < 0 || r > n) return 0;
+        long long ans = (fact[n] * ((invFact[r] % MOD) * (invFact[n - r] % MOD) % MOD)) % MOD;
+        return ans;
+    }
+    long long fun(int i, int m, long long binSum, vector<int>& nums, int k, unordered_map<string, long long>& dp) {
         int n = nums.size();
-        const long long mod = 1e9 + 7;
-        vector<long long> fac(m + 1, 1);
-        for (int i = 1; i <= m; i++) {
-            fac[i] = fac[i - 1] * i % mod;
+        if(m == 0) {
+            return __builtin_popcount(binSum) == k;
         }
-        vector<long long> ifac(m + 1, 1);
-        for (int i = 2; i <= m; i++) {
-            ifac[i] = quickmul(i, mod - 2, mod);
-        }
-        for (int i = 2; i <= m; i++) {
-            ifac[i] = ifac[i - 1] * ifac[i] % mod;
-        }
-        vector numsPower(n, vector<long long>(m + 1, 1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= m; j++) {
-                numsPower[i][j] = numsPower[i][j - 1] * nums[i] % mod;
-            }
-        }
-
-        vector f(n,
-                 vector(m + 1, vector(m * 2 + 1, vector<long long>(k + 1, 0))));
-        for (int j = 0; j <= m; j++) {
-            f[0][j][j][0] = numsPower[0][j] * ifac[j] % mod;
-        }
-        for (int i = 0; i + 1 < n; i++) {
-            for (int j = 0; j <= m; j++) {
-                for (int p = 0; p <= m * 2; p++) {
-                    for (int q = 0; q <= k; q++) {
-                        int q2 = p % 2 + q;
-                        if (q2 > k) {
-                            break;
-                        }
-                        for (int r = 0; r + j <= m; r++) {
-                            int p2 = p / 2 + r;
-                            f[i + 1][j + r][p2][q2] += f[i][j][p][q] *
-                                                       numsPower[i + 1][r] %
-                                                       mod * ifac[r] % mod;
-                            f[i + 1][j + r][p2][q2] %= mod;
-                        }
-                    }
-                }
-            }
-        }
+        if(i >= n) return 0;
+        string str = to_string(i) + '_' + to_string(m) + '_' + to_string(binSum) + '_' + to_string(k);
+        if(dp.find(str) != dp.end()) return dp[str];
+        if(m < 0) return 0;
         long long res = 0;
-        for (int p = 0; p <= m * 2; p++) {
-            for (int q = 0; q <= k; q++) {
-                if (__builtin_popcount(p) + q == k) {
-                    res = (res + f[n - 1][m][p][q] * fac[m] % mod) % mod;
-                }
-            }
+        for(int freq = 1; freq <= m; freq++) {
+            long long newBinSum = binSum + freq;
+            long long prod = fun(i + 1, m - freq, newBinSum >> 1, nums, k - (newBinSum & 1), dp);
+            prod = (prod * (pow(nums[i], freq) * nCrMod(m, freq) % MOD)) % MOD;
+            res = (res + prod) % MOD;
         }
-        return res;
+        long long ntake = fun(i + 1, m, binSum >> 1, nums, k - (binSum & 1), dp);
+        res = (res + ntake) % MOD;
+        return dp[str] = res % MOD;
+    }
+    int magicalSum(int m, int k, vector<int>& nums) {
+        unordered_map<string, long long> dp;
+
+        fact.resize(m + 1, 1);
+        invFact.resize(m + 1, 1);
+        for(int i = 2; i <= m; i++) {
+            fact[i] = (fact[i - 1] * i) % MOD;
+        }
+        for(int i = 0; i <= m; i++) {
+            invFact[i] = pow(fact[i], MOD - 2);
+        }
+        return fun(0, m, 0, nums, k, dp);
     }
 };
