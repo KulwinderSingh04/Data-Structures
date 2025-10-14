e50fb846efe4a363427bde6efcae565d9dee0097
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-14 19:25:12 +0530

    Time: 226 ms (60.73%) | Memory: 189.5 MB (21.45%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count = 1;
-        int a = 0;
-        if(k == 1) return true;
-        for(int i = 1; i < n; i++) {
-            if(nums[i] > nums[i - 1]) count++;
-            else {
-                if(count >= 2 * k) return true;
-                if(count >= k) {
-                    a++;
-                    count = 1;
-                    if(a == 2) return true;
-                } else {
-                    count = 1;
-                    a = 0;
-                }
-            }
-        }
-        if(count >= 2 * k) return true;
-        if(count >= k) {
-            a++;
-            count = 1;
-            if(a == 2) return true;
-        } else {
-            count = 1;
-            a = 0;
-        }
-        return false;
-    }
     int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size();
-        int lo = 1;
-        int hi = n / 2;
-        int ans = 0;
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if(check(nums, mid)) {
-                ans = mid;
-                lo = mid + 1;
+        vector<int> pref(n, 1), suff(n, 1);
+        int count = 1;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] > nums[i - 1]) {
+                count++;
             } else {
-                hi = mid - 1;
+                count = 1;
             }
+            pref[i] = count;
+        }
+        count = 1;
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            suff[i] = count;
+        }
+        int ans = 1;
+        for(int i = 0; i < n - 1; i++) {
+            ans = max(ans, min(pref[i], suff[i + 1]));
         }
         return ans;
     }
