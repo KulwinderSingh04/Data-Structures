5bd2506c220933fb46da3cb6f4bb5db91d5cf6b3
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-25 21:49:40 +0530

    Time: 12 ms (16.67%) | Memory: 214 MB (0.00%) - LeetSync

--- 4042-minimum-operations-to-transform-array/minimum-operations-to-transform-array.cpp
+++ 4042-minimum-operations-to-transform-array/minimum-operations-to-transform-array.cpp
@@ -7,18 +7,15 @@ public:
         for(int i = 0; i < n; i++) {
             if(nums2.back() >= min(nums1[i], nums2[i]) && nums2.back() <= max(nums1[i], nums2[i])) {
                 closest = 0;
-                break;
             }
             if(min(nums1[i], nums2[i]) > nums2.back()) {
                 closest = min(closest, 1LL * min(nums1[i], nums2[i]) - nums2.back());
             } else if(max(nums1[i], nums2[i]) < nums2.back()) {
                 closest = min(closest, 1LL * nums2.back() - max(nums1[i], nums2[i]));
             }
-        }
-        ans += closest + 1;
-        for(int i = 0; i < n; i++) {
             ans += abs(nums1[i] - nums2[i]);
         }
+        ans += closest + 1;
         return ans;
         
     }
