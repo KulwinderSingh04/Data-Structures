30e02162198efad91fdf274c3e7948396ba391ca
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-30 12:48:55 +0530

    Time: 26 ms (52.46%) | Memory: 18.3 MB (86.60%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -5,13 +5,13 @@ public:
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        queue<pp> q;
-        q.push({0, {0, 0}});
+        priority_queue<pp, vector<pp>, greater<pp>> pq;
+        pq.push({0, {0, 0}});
         vector<vector<int>> dist(n, vector<int> (m, INT_MAX));
         dist[0][0] = 0;
-        while(q.size()) {
-            auto t = q.front();
-            q.pop();
+        while(pq.size()) {
+            auto t = pq.top();
+            pq.pop();
             int cost = t.first;
             int r = t.second.first;
             int c = t.second.second;
@@ -22,12 +22,12 @@ public:
                 if(grid[r][c] - 1 == i) {
                     if(dist[nr][nc] > cost) {
                         dist[nr][nc] = cost;
-                        q.push({dist[nr][nc], {nr, nc}});
+                        pq.push({dist[nr][nc], {nr, nc}});
                     }
                 } else {
                     if(dist[nr][nc] > cost + 1) {
                         dist[nr][nc] = cost + 1;
-                        q.push({dist[nr][nc], {nr, nc}});
+                        pq.push({dist[nr][nc], {nr, nc}});
                     }
                 }
             }
