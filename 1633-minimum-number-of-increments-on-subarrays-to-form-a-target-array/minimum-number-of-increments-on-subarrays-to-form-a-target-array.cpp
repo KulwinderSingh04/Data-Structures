207ec9e214f6b0c2b5aa2241c6aa3bf3337cd5b8
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-10-30 12:11:32 +0530

    Time: 362 ms (5.22%) | Memory: 175.5 MB (5.22%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
-    typedef pair<int,int> pp;
+    void buildTree(int i, int l, int r, vector<int>& target, vector<int>& segTree) {
+        if(l == r) {
+            segTree[i] = target[l];
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        buildTree(2 * i + 1, l, mid, target, segTree);
+        buildTree(2 * i + 2, mid + 1, r, target, segTree);
+
+        segTree[i] = min(segTree[2 * i + 1], segTree[2 * i + 2]);
+
+    }
+    int query(int i, int l, int r, int left, int right, vector<int>& segTree) {
+        if(right < l || r < left) return INT_MAX;
+        if(l >= left && r <= right) {
+            return segTree[i];
+        }
+        int mid = l + (r - l) / 2;
+        int a = query(2 * i + 1, l, mid, left, right, segTree);
+        int b = query(2 * i + 2, mid + 1, r, left, right, segTree);
+        return min(a, b);
+
+    }
+    int fun(vector<int>& target, int lo, int hi, int prev, unordered_map<int, vector<int>>& mp, vector<int>& segTree) {
+        if(lo > hi) return 0;
+        int n = target.size();
+        int ele = query(0, 0, n - 1, lo, hi, segTree);
+        int ans = ele - prev;
+        auto& vec = mp[ele];
+        int l = lower_bound(vec.begin(), vec.end(), lo) - vec.begin();
+        int r = upper_bound(vec.begin(), vec.end(), hi) - vec.begin();
+        int prevIdx = lo - 1;
+        for(int i = l; i < r; i++) {
+            ans += fun(target, prevIdx + 1, vec[i] - 1, ele, mp, segTree);
+            prevIdx = vec[i];
+        }
+        ans += fun(target, prevIdx + 1, hi, ele, mp, segTree);
+        return ans;
+    }
     int minNumberOperations(vector<int>& target) {
         int n = target.size();
-        // vector<int> nsi(n), psi(n);
-        // stack<int> st;
-        // for(int i = n - 1; i >= 0; i--) {
-        //     while(st.size() && target[st.top()] >= target[i]) st.pop();
-        //     if(st.size()) nsi[i] = st.top();
-        //     else nsi[i] = n;
-        //     st.push(i);
-        // }
-        // stack<int> gt;
-        // for(int i = 0; i < n; i++) {
-        //     while(gt.size() && target[gt.top()] >= target[i]) gt.pop();
-        //     if(gt.size()) psi[i] = gt.top();
-        //     else psi[i] = -1;
-        //     gt.push(i);
-        // }
-        // vector<pp> v(n);
-        int ans = 0;
-        int prev = 0;
+        vector<int> segTree(4 * n, INT_MAX);
+        buildTree(0, 0, n - 1, target, segTree);
+        unordered_map<int, vector<int>> mp;
         for(int i = 0; i < n; i++) {
-            if(target[i] > prev) ans += target[i] - prev;
-            prev = target[i];
+            mp[target[i]].push_back(i);
         }
-        return ans;
+        return fun(target, 0, n - 1, 0, mp, segTree);
     }
 };
