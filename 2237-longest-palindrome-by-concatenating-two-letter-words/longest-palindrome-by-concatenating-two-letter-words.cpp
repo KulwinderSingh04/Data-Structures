cb11d8c71dffd31b6c9e27dd5b72578d5c04bf83
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-11-13 12:53:26 +0530

    Time: 62 ms (52.42%) | Memory: 171.8 MB (92.11%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        int n = words.size();
-        unordered_map<string, int> m;
+        unordered_map<string, int> mp;
         int ans = 0;
-        for(auto word : words) {
-            string a = word;
-            swap(a[1], a[0]);
-            if(m.find(a) != m.end()) {
-                ans += 4;
-                m[a]--;
-                if(m[a] == 0) m.erase(a);
-            } else {
-                m[word]++;
-            }
+        for(auto x : words) {
+            mp[x]++;
         }
-        for(auto x : m) {
-            if(x.first[0] == x.first[1]) {
-                ans += 2;
-                break;
+        bool flag = true;
+        for(auto x : mp) {
+            cout << x.first << " " << x.second << endl;
+            auto t = x;
+            if(t.first[0] == t.first[1]) {
+                ans += t.second / 2 * 4;
+                if(t.second % 2  && flag) {
+                    ans += 2;
+                    flag = false;
+                }
+            }
+            else {
+                string a = x.first;
+                // cout << a << " ";
+                swap(a[0], a[1]);
+                if(mp.find(a) != mp.end()) {
+                    // cout << x.second << " " << mp[a] << endl;
+                    ans += min(x.second, mp[a]) * 2;
+                    // cout << ans << " ";
+                }
             }
         }
         return ans;
