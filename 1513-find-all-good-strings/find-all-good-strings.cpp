2bc16db763608f73bfbc9dfafbf2d329db9542b0
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-11-21 15:57:49 +0530

    Time: 39 ms (37.61%) | Memory: 12.9 MB (29.91%) - LeetSync

---
+++ 1513-find-all-good-strings/find-all-good-strings.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+    long long fun(int i, int tight, int j, string& s, string& evil, vector<int>& lps, vector<vector<vector<long long>>>& dp) {
+        if(j == evil.size()) return 0;
+        if(i == s.size()) {
+            return 1;
+        }
+        if(dp[i][tight][j] != -1) return dp[i][tight][j];
+        char limit = (tight == 1 ? s[i] : 'z');
+        long long ans = 0;
+        for(char k = 'a'; k <= limit; k++) {
+            int nj = j;
+            while (nj > 0 && evil[nj] != k) nj = lps[nj - 1];
+            if (evil[nj] == k) nj++;
+            ans += fun(i + 1, (tight && (s[i] == k)), nj, s, evil, lps, dp);
+            ans %= MOD;
+        }
+        return dp[i][tight][j] = ans;
+    }
+    vector<int> buildLps(string &evil) {
+        int m = evil.size();
+        vector<int> lps(m, 0);
+
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && evil[i] != evil[j]) j = lps[j - 1];
+            if (evil[i] == evil[j]) j++;
+            lps[i] = j;
+        }
+        return lps;
+    }
+    int findGoodStrings(int n, string s1, string s2, string evil) {
+        vector<int> lps = buildLps(evil);
+        bool flag = true;
+        int ans = 0;
+        for(int i = 0; i < s1.size() - evil.size() + 1; i++) {
+            if(s1.substr(i, evil.size()) == evil) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) ans = 1;
+        vector<vector<vector<long long>>> dp1(s1.size(), vector<vector<long long>> (2, vector<long long> (evil.size(), -1)));
+        vector<vector<vector<long long>>> dp2(s2.size(), vector<vector<long long>> (2, vector<long long> (evil.size(), -1)));
+        return ans + (fun(0, 1, 0, s2, evil, lps, dp1) - fun(0, 1, 0, s1, evil, lps, dp2) + MOD) % MOD;
+    }
+};
