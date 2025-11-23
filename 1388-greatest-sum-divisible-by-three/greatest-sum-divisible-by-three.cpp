e3704a5e234933a2717f649e2f8b5139e7949a81
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-11-23 15:26:47 +0530

    Time: 40 ms (33.86%) | Memory: 46 MB (13.83%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int fun(int i, int rem, vector<int>& nums, vector<vector<int>>& dp) {
-        if(i == nums.size()) return (rem == 0 ? 0 : -1e5);
+        if(i == nums.size()) return (rem == 0 ? 0 : -1e4);
         if(dp[i][rem] != -1) return dp[i][rem];
         int a = nums[i] + fun(i + 1, (rem + nums[i]) % 3, nums, dp);
         int b = fun(i + 1, rem, nums, dp);
