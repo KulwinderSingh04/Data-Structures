97353b060197109cc96dcfabfc62feb5df484614
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-11-29 12:36:26 +0530

    Time: 136 ms (52.84%) | Memory: 188.9 MB (18.80%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        vector<vector<int> > G(n);
-        for(auto edge : edges){
-            G[edge[0]].push_back(edge[1]);
-            G[edge[1]].push_back(edge[0]);
+    long long dfs(int node, vector<vector<int>>& adj, int& k, vector<int>& values, int& ans, vector<int>& vis) {
+        vis[node] = 1;
+        long long a = 0;
+        for(auto x : adj[node]) {
+            if(vis[x] == 0) a += dfs(x, adj, k, values, ans, vis);
         }
-        stack <int> S;
-        vector<int> parent(n);
-        
-        auto DFS = [&](int u, int p, auto &&DFS) -> void{
-            parent[u] = p;
-            S.push(u);
-            for(auto v : G[u]){
-                if(v == p) continue;
-                DFS(v, u, DFS);
-            }
-        };
-        DFS(0, -1, DFS);
-        vector<long long> sum(n);
-        for(int i = 0; i < n; i ++){
-            sum[i] = values[i];
+        a += values[node];
+        if(a % k == 0) {
+            ans++;
+            return 0;
+        }
+        return a;
+    }
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<int> v(n);
+        vector<vector<int>> adj(n);
+        for(auto x : edges) {
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
         }
         int ans = 0;
-        while(!S.empty()){
-            int u = S.top();
-            S.pop();
-            if(sum[u] % k == 0){
-                ans += 1;
-            }
-            else{
-                sum[parent[u]] += sum[u];
-            }
-        }
+        vector<int> vis(n);
+        dfs(0, adj, k, values, ans, vis);
         return ans;
     }
 };
