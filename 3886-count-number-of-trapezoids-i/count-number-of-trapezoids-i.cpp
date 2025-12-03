221060cf93bc68b882b9182c3e9629d0e01f5903
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-12-03 12:22:57 +0530

    Time: 222 ms (17.60%) | Memory: 217.6 MB (21.60%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -1,56 +1,20 @@
-int MOD = 1e9 + 7;
-
-bool cmp(vector<int>& a, vector<int>& b) {
-    return a[1] < b[1];
-}
-
 class Solution {
 public:
-    long long sum(vector<int>& a) {
-        long long totalSum = 0, squareSum = 0;
-        for(int x : a) {
-            totalSum = (totalSum + x) % MOD;
-            squareSum = (squareSum + 1LL * x * x % MOD) % MOD;
-        }
-        long long res = (1LL * totalSum * totalSum % MOD - squareSum + MOD) % MOD;
-        res = (res * 1LL * ((MOD + 1) / 2)) % MOD;
-        return res;
-    }
-
-    long long ncr(int n, int r) {
-        if (r > n - r)
-            r = n - r;
-        long long res = 1;
-        for (int i = 0; i < r; ++i) {
-            res = res * (n - i) % MOD;
-            res = res * modInverse(i + 1) % MOD;
-        }
-        return res;
-    }
-
-    long long modInverse(long long x) {
-        long long res = 1, b = MOD - 2;
-        while (b) {
-            if (b & 1) res = res * x % MOD;
-            x = x * x % MOD;
-            b >>= 1;
-        }
-        return res;
-    }
-
+    int MOD = 1e9 + 7;
     int countTrapezoids(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(), cmp);
-        int count = 1;
-        vector<int> arr;
-        int n = points.size();
-        for(int i = 1; i < n; i++) {
-            if(points[i][1] == points[i - 1][1]) count++;
-            else {
-                if(count != 1) arr.push_back(ncr(count, 2));
-                count = 1;
-            }
+        map<long long, long long> mp;
+        for(auto x : points) {
+            mp[x[1]]++;
         }
-        if(count != 1) arr.push_back(ncr(count, 2));
-        return sum(arr);
+        long long prev = 0;
+        long long ans = 0;
+        for(auto x : mp) {
+            long long lines = x.second;
+            long long hor =((lines * (lines - 1)) % MOD) / 2;
+            ans += hor * prev;
+            ans %= MOD;
+            prev += hor;
+        }
+        return ans;
     }
 };
