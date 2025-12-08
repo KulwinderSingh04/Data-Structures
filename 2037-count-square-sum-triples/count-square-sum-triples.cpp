0a2154f8b6534781fd228cbb54dcfabf77743f45
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-12-08 09:15:43 +0530

    Time: 3 ms (90.49%) | Memory: 7.9 MB (21.68%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -3,11 +3,11 @@ public:
     int countTriples(int n) {
         int ans = 0;
         for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
+            for(int j = i + 1; j <= n; j++) {
                 int k = sqrt(i * i + j * j);
                 if(k * k == i * i + j * j && k <= n) ans++;
             }
         }
-        return ans;
+        return 2 * ans;
     }
 };
