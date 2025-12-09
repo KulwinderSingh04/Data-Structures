aecbd9a9afeaf53b7a3c16b7df28611860e7b66a
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-12-09 10:49:13 +0530

    Time: 8 ms (66.14%) | Memory: 190.4 MB (31.08%) - LeetSync

--- 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
+++ 3755-maximum-product-of-first-and-last-elements-of-a-subsequence/maximum-product-of-first-and-last-elements-of-a-subsequence.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     long long maximumProduct(vector<int>& nums, int m) {
-        long long ans = LLONG_MIN;
         int n = nums.size();
-        vector<long long> posPre(n, nums[0]), negPre(n, nums[0]);
-        // if(nums[0] > 0) posPre[0] = nums[0];/
-        for(int i = 1; i < nums.size(); i++) {
-            posPre[i] = max(posPre[i - 1], 1LL * nums[i]);
+        vector<int> pos(n), neg(n);
+        int mn = INT_MAX, mx = INT_MIN;
+        long long ans = LLONG_MIN;
+        for(int i = n - 1; i >= 0; i--) {
+            mx = max(mx, nums[i]);
+            mn = min(mn, nums[i]);
+            pos[i] = mx;
+            neg[i] = mn;
         }
-        if(nums[0] < 0) posPre[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++) {
-            negPre[i] = min(negPre[i - 1], 1LL * nums[i]);
-        }
-        for(int j = 0; j < n; j++) {
-            if(j >= m - 1) {
-                int idx = j - m + 1;
-                ans = max(ans, 1LL * posPre[idx] * nums[j]);
-                ans = max(ans, 1LL * negPre[idx] * nums[j]);
+        for(int i = 0; i < n - m + 1; i++) {
+            if(nums[i] < 0) {
+                ans = max(ans, 1LL * neg[i + m - 1] * nums[i]);
+            } else {
+                ans = max(ans, 1LL * pos[i + m - 1] * nums[i]);
             }
         }
         return ans;
+
     }
 };
