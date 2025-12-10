f7c70fbf013035574ebfbb5ce81797db4b7d56be
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-12-10 11:05:06 +0530

    Time: 4 ms (36.00%) | Memory: 95.1 MB (18.50%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -3,27 +3,19 @@ public:
     int MOD = 1e9 + 7;
     int countPermutations(vector<int>& complexity) {
         int n = complexity.size();
-        unordered_map<int, int> mp;
+        int c = 0;
         int mn = INT_MAX;
         for(int i = 0; i < n; i++) {
             mn = min(mn, complexity[i]);
-            mp[complexity[i]]++;
+            if(complexity[i] == complexity[0]) c++;
         }
-        if(mp[complexity[0]] > 1 || mn != complexity[0]) return 0;
-        vector<long long> fact(n + 1);
+        if(c > 1 || mn != complexity[0]) return 0;
+        vector<long long> fact(n);
         fact[0] = 1;
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1; i < n; i++) {
             fact[i] = fact[i - 1] * i;
             fact[i] %= MOD;
         }
-        mp[complexity[0]]--;
-        if(mp[complexity[0]] == 0) mp.erase(complexity[0]);
-        long long num = fact[n - 1];
-        // long long deno = 1;
-        // for(auto x : mp) {
-        //     deno *= fact[x.second];
-        //     deno %= MOD;
-        // }
-        return num;
+        return fact[n - 1];
     }
 };
