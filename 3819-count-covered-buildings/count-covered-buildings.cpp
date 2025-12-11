4747a672a64fbb44360a96714b8b6f6b02dc75eb
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-12-11 10:15:30 +0530

    Time: 724 ms (35.36%) | Memory: 347.7 MB (66.85%) - LeetSync

--- 3819-count-covered-buildings/count-covered-buildings.cpp
+++ 3819-count-covered-buildings/count-covered-buildings.cpp
@@ -1,35 +1,21 @@
-
-bool cmp(vector<int>& a, vector<int>& b) {
-    if(a[1] == b[1]) return a[0] < b[0];
-    return a[1] < b[1];
-}
-
 class Solution {
 public:
     int countCoveredBuildings(int n, vector<vector<int>>& buildings) {
-        
-        vector<vector<int>> v1 = buildings;
-        vector<vector<int>> v2 = buildings;
-        sort(v1.begin(), v1.end());
-        sort(v2.begin(), v2.end(), cmp);
-        map<vector<int>, int> m;
+        sort(buildings.begin(), buildings.end());
+        set<vector<int>> st;
+        for(int i = 1; i < buildings.size() - 1; i++) {
+            if(buildings[i - 1][0] == buildings[i][0] && buildings[i][0] == buildings[i + 1][0]) st.insert(buildings[i]);
+        }
+        sort(buildings.begin(), buildings.end(), [](vector<int>& a, vector<int>& b) {
+            if(a[1] == b[1]) return a[0] < b[0];
+            return a[1] < b[1];
+        });
         int ans = 0;
-        for(int i = 0; i < v2.size(); i++) m[v2[i]] = i;
-        for(int i = 1; i < v1.size() - 1; i++) {
-            if(v1[i - 1][0] == v1[i][0] && v1[i + 1][0] == v1[i][0]) {
-                int idx = m[v1[i]];
-                if(v2[idx - 1][1] == v2[idx][1] && v2[idx + 1][1] == v2[idx][1]) {
-                    ans++;
-                }
+        for(int i = 1; i < buildings.size() - 1; i++) {
+            if(buildings[i - 1][1] == buildings[i][1] && buildings[i][1] == buildings[i + 1][1] && st.find(buildings[i]) != st.end()) {
+                ans++;
             }
         }
-        // for(int i = 0; i < v1.size(); i++) {
-        //     cout << v1[i][0] << " " << v1[i][1] << endl;
-        // }
-        // cout << endl;
-        // for(int i = 0; i < v1.size(); i++) {
-        //     cout << v2[i][0] << " " << v2[i][1] << endl;
-        // }
         return ans;
     }
 };
