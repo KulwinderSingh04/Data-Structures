9ba36904727dabcef0d9107b596ac41e99eba1ad
Kulwinder singh <kulwindersingh157679@gmail.com>
2025-12-17 12:52:53 +0530

    Time: 1076 ms (12.58%) | Memory: 344.7 MB (27.02%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    long long fun(int idx, int k, int buy, vector<int>& prices, vector<vector<vector<long long>>>& dp) {
+    long long fun(int i, int k, int buy, vector<int>& prices, vector<vector<vector<long long>>>& dp) {
         int n = prices.size();
-        if(idx == n) {
-            if(buy == 0) return 0;
-            return -1e9;
-        }
-        if(dp[idx][k][buy] != -1) return dp[idx][k][buy];
         if(k == 0) return 0;
+        if(i == n) return buy == 0 ? 0 : -1e16;
+        if(dp[i][k][buy] != -1e16) return dp[i][k][buy];
         if(buy == 0) {
-            long long a = -prices[idx] + fun(idx + 1, k, 1, prices, dp);
-            long long b = prices[idx] + fun(idx + 1, k, 2, prices, dp);
-            long long c = fun(idx + 1, k, 0, prices, dp);
-            return dp[idx][k][buy] = max(a, max(b, c));
+            long long a = -prices[i] + fun(i + 1, k, 1, prices, dp);
+            long long b = fun(i + 1, k, 0, prices, dp);
+            long long c = prices[i] + fun(i + 1, k, 2, prices, dp);
+            return dp[i][k][buy] = max({a, b, c});
         } else if(buy == 1) {
-            long long a = prices[idx] + fun(idx + 1, k - 1, 0, prices, dp);
-            long long b = fun(idx + 1, k, 1, prices, dp);
-            return dp[idx][k][buy] = max(a, b);
+            long long a = prices[i] + fun(i + 1, k - 1, 0, prices, dp);
+            long long b = fun(i + 1, k, 1, prices, dp);
+            return dp[i][k][buy] = max(a, b);
         } else {
-            long long a = -prices[idx] + fun(idx + 1, k - 1, 0, prices, dp);
-            long long b = fun(idx + 1, k , 2, prices, dp);
-            return dp[idx][k][buy] = max(a, b);
+            long long c = -prices[i] + fun(i + 1, k - 1, 0, prices, dp);
+            long long b = fun(i + 1, k, 2, prices, dp);
+            return dp[i][k][buy] = max(b, c);   
         }
-        
     }
     long long maximumProfit(vector<int>& prices, int k) {
         int n = prices.size();
-        vector<vector<vector<long long>>> dp(n, vector<vector<long long>> (k + 1, vector<long long> (3, -1)));
+        vector<vector<vector<long long>>> dp(n, vector<vector<long long>> (k + 1, vector<long long> (3, -1e16)));
         return fun(0, k, 0, prices, dp);
     }
 };
