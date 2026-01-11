c29db52baaa600fdac02aa7b3fae744770aa6b91
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-01-11 14:41:48 +0530

    Time: 34 ms (75.42%) | Memory: 66.4 MB (19.82%) - LeetSync

--- 15-3sum/3sum.cs
+++ 15-3sum/3sum.cs
@@ -1,28 +1,24 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
-        IList<IList<int>> li = new List<IList<int>>();
-        HashSet<(int, int, int)> hs = new HashSet<(int, int, int)>();
-        int n = nums.Length;
         Array.Sort(nums);
+        int n = nums.Length;
+        IList<IList<int>> li = new List<IList<int>>();
         for(int i = 0; i < n; i++) {
-            int newTarget = -nums[i];
-            Dictionary<int, int> dt = new Dictionary<int, int>();
-            for(int j = i + 1; j < n; j++) {
-                if(dt.ContainsKey(newTarget - nums[j])) {
-                    List<int> v = new List<int>();
-                    hs.Add((nums[i], newTarget - nums[j], nums[j]));
-                }
-                dt[nums[j]] = j;
+            if(i != 0 && nums[i] == nums[i - 1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0) {
+                    li.Add(new List<int> {nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
+                } else if(sum < 0) {
+                    j++;
+                } else k--;
             }
-        }
-        HashSet<string> hst = new HashSet<string>();
-        
-        foreach((int, int, int) x in hs) {
-            List<int> v = new List<int>();
-            v.Add(x.Item1);
-            v.Add(x.Item2);
-            v.Add(x.Item3);
-            li.Add(v);
         }
         return li;
     }
