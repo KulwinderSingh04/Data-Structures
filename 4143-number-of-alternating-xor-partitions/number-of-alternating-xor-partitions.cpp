e121a4d318fa68488f394d8ba9219ff26995b7e5
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-01-17 23:25:39 +0530

    Time: 1176 ms (5.03%) | Memory: 448.2 MB (5.03%) - LeetSync

--- 4143-number-of-alternating-xor-partitions/number-of-alternating-xor-partitions.cpp
+++ 4143-number-of-alternating-xor-partitions/number-of-alternating-xor-partitions.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
-    static const int MOD = 1e9 + 7;
-    unordered_map<long long, int> memo;
-
-    /*
-      index        -> current position in nums
-      currentXor   -> XOR accumulated so far
-      isTeam1Turn  -> true if Team 1's turn, false if Team 2's turn
-    */
-    int solve(int index,int currentXor,bool isT1Turn,vector<int>& nums,int t1,int t2) {
-        // Base case: reached end of array
-        if (index == nums.size()) {
-            if ((isT1Turn && currentXor == t1) ||
-                (!isT1Turn && currentXor == t2)) {
-                return 1;
-            }
-            return 0;
+    int MOD = 1e9 + 7;
+    long long fun(int i, int x, int toggle, vector<int>& nums, vector<int>& v, unordered_map<long long, int>& dp) {
+        int n = nums.size();
+        x ^= nums[i];
+        if(i == n - 1) return x == v[toggle];
+        long long stateKey =((long long)i << 32) |((long long)x << 1) | toggle;
+        if(dp.find(stateKey) != dp.end()) return dp[stateKey];
+        long long a = 0;
+        if(x == v[toggle]) {
+            a = fun(i + 1, 0, !toggle, nums, v, dp);
         }
-
-        // Encode state into a single key for memoization
-        long long stateKey =((long long)index << 32) |((long long)currentXor << 1) | isT1Turn;
-
-        if (memo.count(stateKey))
-            return memo[stateKey];
-
-        long long ways = 0;
-
-        // Option 1: Stop current segment & switch turn
-        if ((isT1Turn && currentXor == t1) ||
-            (!isT1Turn && currentXor == t2)) {
-            ways += solve(index + 1,nums[index],!isT1Turn,nums,t1,t2);
-        }
-
-        // Option 2: Continue XOR with same num
-        ways += solve(index + 1,currentXor ^ nums[index],isT1Turn,nums,t1,t2);
-
-        return memo[stateKey] = ways % MOD;
+        long long b = fun(i + 1, x, toggle, nums, v, dp);
+        return dp[stateKey] = (a + b) % MOD;
     }
-
     int alternatingXOR(vector<int>& nums, int target1, int target2) {
-        // Start from index 1, with nums[0] as initial XOR
-        return solve(1, nums[0], true, nums, target1, target2);
+        vector<int> v = {target1, target2};
+        // int n = nums.size();
+        // for(int i = 1; i < n; i++) {
+        //     nums[i] = nums[i - 1] ^ nums[i];
+        // }
+        unordered_map<long long, int> dp;
+        return fun(0, 0, 0, nums, v, dp);
+        
     }
 };
