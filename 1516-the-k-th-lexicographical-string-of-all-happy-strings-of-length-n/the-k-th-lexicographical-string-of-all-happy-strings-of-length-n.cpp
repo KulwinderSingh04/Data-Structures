d3e8b5465f57b2ab00e046d8b04b5383ce824d71
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-03-14 12:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (73.48%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        string ans = "";
+        if(3 * (1 << (n - 1)) < k) return "";
+        for(int i = 0; i < n; i++) {
+            if(i == 0) {
+                int div = k / (1 << (n - 1 - i)) - 1;
+                if(k % (1 << (n - 1 - i)) == 0) ans += div + 97;
+                else ans += div + 98;
+                if(ans.back() > 'c') return "";
+                k = k % (1 << (n - 1 - i));
+                if(k == 0) k = (1 << (n - 1 - i));
+            } else {
+                vector<int> v;
+                if(ans.back() == 'a') v = {'b', 'c'};
+                else if(ans.back() == 'b') v = {'a', 'c'};
+                else v = {'a', 'b'};
+                int div = k / (1 << (n - 1 - i)) - 1;
+                if(k % (1 << (n - 1 - i)) == 0) ans += v[div];
+                else {
+                    if(div + 1 < 2) ans += v[div + 1]; 
+                    else return "";
+                } 
+                if(ans.back() > 'c') return "";
+                k = k % (1 << (n - 1 - i));
+                if(k == 0) k = (1 << (n - 1 - i));
+            }
+        }
+        return ans;
+    }
+};
