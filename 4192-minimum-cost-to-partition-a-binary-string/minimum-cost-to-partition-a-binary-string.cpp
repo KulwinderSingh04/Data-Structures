708b56e9e4d841c8bcc1c6107e4f7ab54ea6c07d
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-03-14 15:23:58 +0530

    Time: 96 ms (45.13%) | Memory: 88.7 MB (81.29%) - LeetSync

--- 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.cpp
+++ 4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    long long fun(int l, int r, string& s, int& encCost, int& flatCost) {
+    long long fun(int l, int r, vector<int>& v, int& encCost, int& flatCost) {
         if(l > r) return 1e17;
-        int sen = 0;
-        for(int i = l; i <= r; i++) {
-            sen += s[i] == '1';
-        }
+        int sen = v[r];
+        if(l > 0) sen -= v[l - 1];
         long long cost = 0;
         if(sen == 0) cost = flatCost;
         else cost = 1LL * (r - l + 1) * sen * encCost;
         if((r - l + 1) % 2 == 0) {
             int mid = l + (r - l) / 2;
-            cost = min(cost, fun(l, mid, s, encCost, flatCost) + fun(mid + 1, r, s, encCost, flatCost));
+            cost = min(cost, fun(l, mid, v, encCost, flatCost) + fun(mid + 1, r, v, encCost, flatCost));
         }
         return cost;
     }
     long long minCost(string s, int encCost, int flatCost) {
         int n = s.size();
-        return fun(0, n - 1, s, encCost, flatCost);
+        vector<int> v(n);
+        for(int i = 0; i < n; i++) {
+            v[i] = s[i] == '1';
+            if(i > 0) v[i] += v[i - 1];
+        }
+        return fun(0, n - 1, v, encCost, flatCost);
     }
 };
