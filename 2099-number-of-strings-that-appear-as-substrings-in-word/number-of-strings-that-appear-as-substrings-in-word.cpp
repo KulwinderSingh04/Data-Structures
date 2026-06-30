9b0692388a32bab4e668c253d74a8bd4998f64f4
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-06-30 17:07:26 +0530

    Time: 4 ms (15.31%) | Memory: 13.2 MB (9.81%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -1,14 +1,46 @@
 class Solution {
 public:
+    vector<int> fun(string& patt) {
+        int n = patt.size();
+        int j = 1;
+        int len = 0;
+        vector<int> lps(n);
+        while(j < n) {
+            if(patt[j] == patt[len]) {
+                lps[j] = len + 1;
+                len++;
+                j++;
+            } else {
+                if(len) {
+                    len = lps[len - 1];
+                } else {
+                    j++;
+                }
+            }
+        }
+        return lps;
+    }
+    bool match(string& word, string& patt) {
+        vector<int> lps = fun(patt);
+        int n1 = word.size();
+        int n2 = patt.size();
+        int i = 0, j = 0;
+        while(i < n1) {
+            if(word[i] == patt[j]) {
+                i++;
+                j++;
+                if(j == n2) return true;
+            } else {
+                if(j != 0) j = lps[j - 1];
+                else i++;
+            }
+        }
+        return false;
+    }
     int numOfStrings(vector<string>& patterns, string word) {
         int cnt = 0;
         for(auto x : patterns) {
-            for(int i = 0; i < word.size(); i++) {
-                if(word[i] == x[0] && word.substr(i, x.size()) == x) {
-                    cnt++;
-                    break;
-                }
-            }
+            if(match(word, x)) cnt++;
         }
         return cnt;
     }
