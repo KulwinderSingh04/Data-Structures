032ccd543baef581e79b395656f65cca21b49bf1
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-07-11 12:40:24 +0530

    Time: 501 ms (12.80%) | Memory: 419.7 MB (7.92%) - LeetSync

--- 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.cpp
+++ 3852-path-existence-queries-in-a-graph-ii/path-existence-queries-in-a-graph-ii.cpp
@@ -1,43 +1,65 @@
 class Solution {
 public:
-    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<pair<int, int>> newNums(n);
-        for (int i = 0; i < n; i++)
-            newNums[i] = {nums[i], i};
-        sort(newNums.begin(), newNums.end());
-
-        vector<int> getI(n);
-        for (int i = 0; i < n; i++)
-            getI[newNums[i].second] = i;
-
-        vector<vector<int>> st(n, vector<int>(18));
-
-        int r = 0;
-        for (int i = 0; i < n; i++) {
-            if (r < i) r = i;
-            while (r + 1 < n &&
-                   newNums[r + 1].first - newNums[r].first <= maxDiff &&
-                   newNums[r + 1].first - newNums[i].first <= maxDiff)
-                r++;
-            st[i][0] = r;
+    typedef pair<int, int> pp;
+    int customUpperBound(vector<pp>& arr, int mx) {
+        int n = arr.size();
+        int lo = 0;
+        int hi = n - 1;
+        int ans = 0;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(arr[mid].first <= mx) {
+                ans = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
-
-        for (int j = 1; j < 18; j++)
-            for (int i = 0; i < n; i++)
-                st[i][j] = st[st[i][j - 1]][j - 1];
-
-        vector<int> ans(queries.size(), -1);
-        for (int i = 0; i < (int)queries.size(); i++) {
-            int a = getI[queries[i][0]];
-            int b = getI[queries[i][1]];
-            if (a > b) swap(a, b);
-            if (a == b) { ans[i] = 0; continue; }
-
-            int curr = a, steps = 0;
-            for (int j = 17; j >= 0; j--)
-                if (st[curr][j] < b) { curr = st[curr][j]; steps += (1 << j); }
-
-            ans[i] = (st[curr][0] >= b) ? steps + 1 : -1;
+        return ans;
+    }
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<pp> arr;
+        for(int i = 0; i < nums.size(); i++) {
+            arr.push_back({nums[i], i});
+        }
+        sort(arr.begin(), arr.end());
+        vector<int> nodeToIdx(n);
+        for(int i = 0; i < n; i++) {
+            nodeToIdx[arr[i].second] = i;
+        }
+        int rows = n;
+        int cols = log2(n) + 1;
+        vector<vector<int>> up(rows, vector<int> (cols, -1));
+        for(int node = 0; node < n; node++) {
+            up[node][0] = customUpperBound(arr, arr[node].first + maxDiff);
+        }
+        for(int j = 1; j < cols; j++) {
+            for(int node = 0; node < n; node++) {
+                if(up[node][j - 1] != -1) {
+                    up[node][j] = up[up[node][j - 1]][j - 1];
+                }
+            }
+        }
+        vector<int> ans;
+        for(auto q : queries) {
+            int a = nodeToIdx[q[0]];
+            int b = nodeToIdx[q[1]];
+            if(a > b) swap(a, b);
+            if(a == b) {
+                ans.push_back(0);
+                continue;
+            }
+            int curr = a;
+            int jumps = 0;
+            for(int j = cols - 1; j >= 0; j--) {
+                if(up[curr][j] < b) {
+                    curr = up[curr][j];
+                    jumps += (1 << j);
+                }
+            }
+            if(up[curr][0] >= b) {
+                ans.push_back(jumps + 1);
+            } else ans.push_back(-1);
         }
         return ans;
     }
