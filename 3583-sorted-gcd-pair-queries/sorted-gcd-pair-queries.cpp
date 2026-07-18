884fb778835cdf8c9cf5c0938dd2cb1fdb188a2e
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-07-18 15:26:47 +0530

    Time: 105 ms (36.14%) | Memory: 123 MB (77.11%) - LeetSync

--- 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
+++ 3583-sorted-gcd-pair-queries/sorted-gcd-pair-queries.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
-        int m = *max_element(nums.begin(), nums.end());
-        vector<long long> cnt(m + 1);
-        for (int num : nums) {
-            cnt[num]++;
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = i * 2; j <= m; j += i) {
-                cnt[i] += cnt[j];
+        int mx = *max_element(nums.begin(), nums.end());
+        vector<long long> gcdPairCount(mx + 1);
+        for(auto x : nums) {
+            for(int j = 1; j * j <= x; j++) {
+                if(x % j == 0) {
+                    int r1 = j;
+                    int r2 = x / j;
+                    gcdPairCount[r1]++;
+                    if(r1 != r2) gcdPairCount[r2]++;
+                }
             }
         }
-        for (int i = 1; i <= m; i++) {
-            cnt[i] = cnt[i] * (cnt[i] - 1) / 2;
-        }
-        for (int i = m; i >= 1; i--) {
-            for (int j = i * 2; j <= m; j += i) {
-                cnt[i] -= cnt[j];
+        for(int i = mx; i >= 1; i--) {
+            gcdPairCount[i] = (1LL * gcdPairCount[i] * (gcdPairCount[i] - 1) / 2);
+            for(int j = 2 * i; j <= mx; j += i) {
+                gcdPairCount[i] -= gcdPairCount[j];
             }
         }
-        for (int i = 1; i <= m; i++) {
-            cnt[i] += cnt[i - 1];
-        }
+        for(int i = 1; i < mx + 1; i++) gcdPairCount[i] += gcdPairCount[i - 1];
+        // for(int i = 1; i < mx + 1; i++) gcdPairCount[i]--;
+        // return gcdPairCount;
         vector<int> ans;
-        for (long long q : queries) {
-            q++;
-            int pos = lower_bound(cnt.begin(), cnt.end(), q) - cnt.begin();
-            ans.push_back(pos);
+        for(auto q : queries) {
+            int idx = lower_bound(gcdPairCount.begin() + 1, gcdPairCount.end(), q + 1) - gcdPairCount.begin();
+            // cout << idx << " ";
+            // if(gcdPairCount[idx] == q) idx++;
+            ans.push_back(idx);
         }
         return ans;
     }
