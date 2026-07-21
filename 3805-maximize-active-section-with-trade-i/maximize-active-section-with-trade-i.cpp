e16a16df326ae7c677d12e528be5459b329dfe82
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-07-21 15:36:44 +0530

    Time: 128 ms (19.37%) | Memory: 117.6 MB (27.48%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n = s.size() + 2;
-        reverse(s.begin(), s.end());
-        s.push_back('1');
-        reverse(s.begin(), s.end());
-        s.push_back('1');
-        int c1 = 0;
-        int c0 = 0;
-        int a = 0;
-        vector<pair<int, int>> ans;
-        for(int i = 0; i < n; i++) {
-            if(s[i] == '1') {
-                a++;
-                c1++;
-                if(c0 != 0) {
-                    ans.push_back({0, c0});
-                    c0 = 0;
-                }
-            }
+        vector<int> v;
+        int n = s.size();
+        int cnt = 1;
+        string str = "";
+        str += s[0];
+        for(int i = 1; i < n; i++) {
+            if(s[i] == s[i - 1]) cnt++;
             else {
-                c0++;
-                if(c1 != 0) {
-                    ans.push_back({1, c1});
-                    c1 = 0;
+                str += s[i];
+                v.push_back(cnt);
+                cnt = 1;
+            }
+        }
+        v.push_back(cnt);
+        int c1 = 0;
+        for(auto x : s) c1 += (x == '1');
+        int ans = c1;
+        for(int i = 1; i < v.size() - 1; i++) {
+            if(str[i] == '1') {
+                int a = 0;
+                if(i - 1 >= 0 && i + 1 < v.size()) {
+                    a = v[i] + v[i - 1] + v[i + 1];
+                    ans = max(ans, a + c1 - v[i]);
                 }
             }
         }
-        if(c1 != 0) {
-            ans.push_back({1, c1});
-            c1 = 0;
-        }
-        int res = 0;
-        // for(int i = 0; i < n; i++) cout << ans[i].first << " " << ans[i].second << endl;
-        for(int i = 1; i < ans.size(); i += 2) {
-            if(i + 2 < ans.size()) res = max(ans[i].second + ans[i + 2].second, res);
-        }
-        return res + a - 2;
+        return ans;
     }
 };
