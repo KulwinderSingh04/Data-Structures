38e3658fbedcbd70b3ae04cd617035ddc2a434aa
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-01 11:17:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (16.44%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    long long fun(int i, int j, int turn, vector<int>& nums, vector<vector<vector<long long>>>& dp) {
+    long long fun(int i, int j, int turn, vector<int>& nums, vector<vector<long long>>& dp) {
         if(i > j) return 0;
-        if(dp[i][j][turn] != -1e17) return dp[i][j][turn];
+        if(dp[i][j] != -1e17) return dp[i][j];
         if(turn) {
             long long a = -1e16;
             a = max(a, nums[i] + fun(i + 1, j, !turn, nums, dp));
             a = max(a, nums[j] + fun(i, j - 1, !turn, nums, dp));
-            return dp[i][j][turn] = a;
+            return dp[i][j] = a;
         } else {
             long long a = 1e16;
             a = min(a, -nums[i] + fun(i + 1, j, !turn, nums, dp));
             a = min(a, -nums[j] + fun(i, j - 1, !turn, nums, dp));
-            return dp[i][j][turn] = a;
+            return dp[i][j] = a;
         }
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<vector<long long>>> dp(n, vector<vector<long long>> (n, vector<long long> (2, -1e17)));
+        vector<vector<long long>> dp(n, vector<long long> (n, -1e17));
         return fun(0, n - 1, 1, nums, dp) >= 0;
     }
 };
