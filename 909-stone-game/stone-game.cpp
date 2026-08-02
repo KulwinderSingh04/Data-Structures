2b6b3ce72f614db1d160aa6b97ed2bc22c72fd0f
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-02 09:46:43 +0530

    Time: 27 ms (5.74%) | Memory: 28.2 MB (5.70%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        return true;
+    long long fun(int i, int j, int turn, vector<int>& nums, vector<vector<long long>>& dp) {
+        if(i > j) return 0;
+        if(dp[i][j] != -1e17) return dp[i][j];
+        if(turn) {
+            long long a = -1e16;
+            a = max(a, nums[i] + fun(i + 1, j, !turn, nums, dp));
+            a = max(a, nums[j] + fun(i, j - 1, !turn, nums, dp));
+            return dp[i][j] = a;
+        } else {
+            long long a = 1e16;
+            a = min(a, -nums[i] + fun(i + 1, j, !turn, nums, dp));
+            a = min(a, -nums[j] + fun(i, j - 1, !turn, nums, dp));
+            return dp[i][j] = a;
+        }
+    }
+    bool stoneGame(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<long long>> dp(n, vector<long long> (n, -1e17));
+        return fun(0, n - 1, 1, nums, dp) >= 0;
     }
 };
