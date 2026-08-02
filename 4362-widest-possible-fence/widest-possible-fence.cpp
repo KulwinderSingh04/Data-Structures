cd577357cafeb4343353a6cbec6a5e3e967b60a0
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-02 12:16:58 +0530

    Time: 1424 ms (100.00%) | Memory: 331.3 MB (88.89%) - LeetSync

--- 4362-widest-possible-fence/widest-possible-fence.cpp
+++ 4362-widest-possible-fence/widest-possible-fence.cpp
@@ -13,12 +13,12 @@ public:
         }
         // sort(v.begin(), v.end());
         for(int i = 0; i < v.size(); i++) {
-            mp[2 * v[i]] += mp2[v[i]] / 2;
-            for(int j = i + 1; j < v.size(); j++) {
-                mp[v[i] + v[j]] += min(mp2[v[i]], mp2[v[j]]);
+            for(int j = i; j < v.size(); j++) {
+                if(i == j) mp[2 * v[i]] += mp2[v[i]] / 2;
+                else mp[v[i] + v[j]] += min(mp2[v[i]], mp2[v[j]]);
             }
         }
-        int ans = 0;
+        int ans =0;
         for(auto x : mp) ans = max(x.second, ans);
         return ans;
     }
