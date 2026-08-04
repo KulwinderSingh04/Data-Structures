1d7dfbf8b3eaeeebe5b51eda708ee89d04f47e0d
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-04 18:34:48 +0530

    Time: 347 ms (82.35%) | Memory: 73.7 MB (28.24%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -1,31 +1,39 @@
 class Solution {
 public:
-    void fun(int i, int sum, int k, vector<vector<int>>& ans, vector<int>& nums, int e) {
+    void fun(int i, int sum, int k, vector<int>& nums, vector<vector<int>>& v) {
         int n = nums.size();
-        if(i == e) {
-            ans[k].push_back(sum);
+        if(i == n) {
+            v[k].push_back(sum);
             return;
         }
-        fun(i + 1, sum + nums[i], k + 1, ans, nums, e);
-        fun(i + 1, sum, k, ans, nums, e);
+        fun(i + 1, sum + nums[i], k + 1, nums, v);
+        fun(i + 1, sum, k, nums, v);
     }
     int minimumDifference(vector<int>& nums) {
         int n = nums.size();
-        int totSum = 0;
-        for(auto x : nums) totSum += x;
-        vector<vector<int>> v1(n / 2 + 1), v2(n / 2 + 1);
-        fun(0, 0, 0, v1, nums, n / 2);
-        fun(n / 2, 0, 0, v2, nums, n);
-        for(int i = 0; i < v2.size(); i++) {
-            sort(v2[i].begin(), v2[i].end());
+        vector<int> nums1, nums2;
+        int tot = accumulate(nums.begin(), nums.end(), 0);
+        for(int i = 0; i < n; i++) {
+            if(i < n / 2) nums1.push_back(nums[i]);
+            else nums2.push_back(nums[i]);
         }
-        int ans = INT_MAX;
-        for(int k = 1; k < v1.size(); k++) {
-            for(int i = 0; i < v1[k].size(); i++) {
-                auto l = lower_bound(v2[n / 2 - k].begin(), v2[n / 2 - k].end(),(totSum - 2 * v1[k][i]) / 2); 
-                int idx = l - v2[n / 2 - k].begin();
-                if(idx < v2[n / 2 - k].size()) ans = min(ans, abs(totSum - 2 * (v1[k][i] + v2[n / 2 - k][idx])));
-                if(idx - 1 >= 0) ans = min(ans, abs(totSum - 2 * (v1[k][i] + v2[n / 2 - k][idx - 1])));
+        vector<vector<int>> v1(n / 2 + 1), v2(n / 2 + 1);
+        fun(0, 0, 0, nums1, v1);
+        fun(0, 0, 0, nums2, v2);
+        for(auto& x : v1) {
+            sort(x.begin(), x.end());
+        }
+        for(auto& x : v2) {
+            sort(x.begin(), x.end());
+        }
+        int ans = 1e9;
+        for(int i = 0; i < n / 2 + 1; i++) {
+            for(auto x : v1[i]) {
+                int leftSum = x;
+                int rightSum = (tot - 2 * leftSum) / 2;
+                int idx = lower_bound(v2[n / 2 - i].begin(), v2[n / 2 - i].end(), rightSum) - v2[n / 2 - i].begin();
+                if(idx < v2[n / 2 - i].size()) ans = min(ans, abs(tot - 2 * (leftSum + v2[n / 2 - i][idx])));
+                if(idx - 1 >= 0) ans = min(ans, abs(tot - 2 * (leftSum + v2[n / 2 - i][idx - 1])));
             }
         }
         return ans;
