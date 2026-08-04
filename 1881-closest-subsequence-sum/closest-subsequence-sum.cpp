27fb28838a16c20e0730fd079751075282eadade
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-04 13:40:24 +0530

    Time: 512 ms (84.63%) | Memory: 45.5 MB (98.32%) - LeetSync

--- 1881-closest-subsequence-sum/closest-subsequence-sum.cpp
+++ 1881-closest-subsequence-sum/closest-subsequence-sum.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    void fun(int i, int sum, vector<int>& ans, vector<int>& nums, int e) {
+    void fun1(int i, int sum, vector<int>& nums, vector<int>& v) {
         int n = nums.size();
-        if(i == e) {
-            ans.push_back(sum);
+        if(i == n) {
+            v.push_back(sum);
             return;
         }
-        fun(i + 1, sum + nums[i], ans, nums, e);
-        fun(i + 1, sum, ans, nums, e);
+        fun1(i + 1, sum + nums[i], nums, v);
+        fun1(i + 1, sum, nums, v);
     }
+    void fun2(int i, int sum, vector<int>& nums, vector<int>& v, int& ans) {
+        int n = nums.size();
+        if(i == n) {
+            int idx = lower_bound(v.begin(), v.end(), sum) - v.begin();
+            if(idx < v.size()) ans = min(ans, abs(sum - v[idx]));
+            if(idx - 1 >= 0) ans = min(ans, abs(sum - v[idx - 1]));
+            return;
+        }
+        fun2(i + 1, sum - nums[i], nums, v, ans);
+        fun2(i + 1, sum, nums, v, ans);
+    }
+
     int minAbsDifference(vector<int>& nums, int goal) {
         int n = nums.size();
-        vector<int> v1, v2;
-        fun(0, 0, v1, nums, n / 2);
-        fun(n / 2, 0, v2, nums, n);
-        sort(v2.begin(), v2.end());
-        int ans = INT_MAX;
-        for(int i = 0; i < v1.size(); i++) {
-            auto l = lower_bound(v2.begin(), v2.end(), goal - v1[i]);
-            int idx = l - v2.begin();
-            if(idx < v2.size()) ans = min(ans, abs(v2[idx] + v1[i] - goal));
-            if(idx - 1 >= 0) ans = min(ans, abs(v2[idx - 1] + v1[i] - goal));
+        vector<int> v, nums1, nums2;
+        for(int i = 0; i < n; i++) {
+            if(i < n / 2) nums1.push_back(nums[i]);
+            else nums2.push_back(nums[i]);
         }
+        fun1(0, 0, nums1, v);
+        sort(v.begin(), v.end());
+        int ans = 1e9;
+        fun2(0, goal, nums2, v, ans);
         return ans;
     }
 };
