ae309ee181e6242b141a0c088be1568fea7a6fe5
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-09 12:22:06 +0530

    Time: 22 ms (25.78%) | Memory: 34.9 MB (5.53%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int fun(int i, vector<int>& piles, int m, int flag, vector<vector<vector<int>>>& dp) {
+    int fun(int i, int turn, int m, vector<int>& piles, vector<vector<vector<int>>>& dp) {
         int n = piles.size();
         if(i == n) return 0;
-        if(dp[i][m][flag] != -1) return dp[i][m][flag];
-        int p;
-        if(flag) p = 0;
-        else p = INT_MAX;
         int sum = 0;
-        for(int j = i; j < min(n, i + 2 * m); j++) {
-            sum += piles[j];
-            int t = fun(j + 1, piles, max(m, j - i + 1), !flag, dp);
-            if(flag == 1) p = max(sum + t, p);
-            else p = min(p, t);
+        int ans = 0;
+        if(dp[i][turn][m] != -1) return dp[i][turn][m];
+        if(turn == 1) ans = 1e9;
+        if(turn == 0) {
+            // int ans = 0;
+            for(int j = i; j < min(n, i + 2 * m); j++) {
+                int x = j - i + 1;
+                sum += piles[j];
+                ans = max(ans, sum + fun(j + 1, !turn, max(m, x), piles, dp));
+            }
+        } else {
+            // int ans = 1e9;
+            for(int j = i; j < min(n, i + 2 * m); j++) {
+                int x = j - i + 1;
+                sum += piles[j];
+                ans = min(ans, fun(j + 1, !turn, max(m, x), piles, dp));
+            }
         }
-        return dp[i][m][flag] = p;
+        return dp[i][turn][m] = ans;
     }
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (250, vector<int> (2, -1)));
-        return fun(0, piles, 1, 1, dp);
+        vector<vector<vector<int>>> dp(n, vector<vector<int>> (2, vector<int> (500, -1)));
+        return fun(0, 0, 1, piles, dp);
     }
 };
