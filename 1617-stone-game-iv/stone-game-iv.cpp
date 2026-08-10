692ea993c1108be568007d3a6651babb6d7bb4bc
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-10 11:15:25 +0530

    Time: 213 ms (13.77%) | Memory: 39.6 MB (6.78%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.cpp
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    bool fun(int n, int alice, vector<int>& v, vector<vector<int>>& dp) {
-        if(n == 0) return alice == 1;
+    bool fun(int n, int alice, vector<vector<int>>& dp) {
+        if(n == 0) return alice == 0;
         if(dp[n][alice] != -1) return dp[n][alice];
-        for(int i = 0; i < v.size(); i++) {
-            if(v[i] <= n) {
-                bool a = fun(n - v[i], !alice, v, dp);
-                if(alice == 0) {
-                    if(a) return dp[n][alice] = a;
-                } else {
-                    if(a == false) return dp[n][alice] = a;
-                }
-            } else break;
+        if(alice) {
+            for(int i = 1; i * i <= n; i++) {
+                if(fun(n - i * i, !alice, dp)) return dp[n][alice] = true;
+            }
+            return dp[n][alice] = false;
+        } else {
+            for(int i = 1; i * i <= n; i++) {
+                if(!fun(n - i * i, !alice, dp)) return dp[n][alice] = false;
+            }
+            return dp[n][alice] = true;
         }
-        if(alice == 0) return dp[n][alice] = false;
-        else return dp[n][alice] = true;
     }
     bool winnerSquareGame(int n) {
-        vector<int> v;
-        int num = 1;
-        while(num * num <= 1e5) {
-            v.push_back(num * num);
-            num++;
-        }
-        // cout << v.size();
         vector<vector<int>> dp(n + 1, vector<int> (2, -1));
-        return fun(n, 0, v, dp);
+        return fun(n, 1, dp);
     }
 };
