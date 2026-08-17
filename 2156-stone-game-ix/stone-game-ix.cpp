9b1abc01d6c00d7829d6897ebae7a4dd9264d558
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-17 12:04:19 +0530

    Time: 0 ms (100.00%) | Memory: 131.2 MB (93.11%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.cpp
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -1,14 +1,52 @@
 class Solution {
 public:
+    bool fun1(vector<int> v) {
+        if(v[1] == 0) return false;
+        if(v[1] == 1) {
+            if(v[2] == 0) return false;
+            return v[0] % 2 == 0;
+        }
+        v[1] -= 2;
+        int mn = min(v[1], v[2]);
+        v[1] -= mn;
+        v[2] -= mn;
+        // cout << v[0] << "-" << v[1] << " " << v[2] << endl;
+        if(v[2]) {
+            v[2]--;
+            if(v[2] == 0) return false;
+            return v[0] % 2 == 0;
+            // int mn2 = min(v[2], v[0]);
+            // // cout << mn2 << endl;
+            // v[2] -= mn2;
+            // v[0] -= mn2;
+            // // cout << v[0] << " " << v[1] << " " << v[2] << endl;
+            // if(v[0] == 0 && v[1] == 0 && v[2] == 0) return false;
+            // if(v[0]) {
+            //     return false;
+            // }
+            // if(v[2]) {
+            //     return true;
+            // }
+        }
+        v[0] -= v[0] / 2 * 2;
+        if(v[0] == 0 && v[1] == 0 && v[2] == 0) return false;
+        if(v[0]) {
+            v[0]--;
+            if(v[0] == 0 && v[1] == 0 && v[2] == 0) return false;
+            return true;
+        }
+        return false;
+    }
     bool stoneGameIX(vector<int>& stones) {
-        int f[3] = {0, 0, 0};
-
-        for (auto& s : stones)
-            f[s % 3]++;
-
-        if (~f[0] & 1)
-            return min(f[1], f[2]) >= 1;
-
-        return abs(f[1] - f[2]) >= 3;
+        if(stones.size() == 1) return false;
+        vector<int> v(3);
+        for(auto x : stones) {
+            v[x % 3]++;
+        }
+        bool a = fun1(v);
+        swap(v[1], v[2]);
+        bool b = fun1(v);
+        cout << a << " " << b;
+        return a || b;
     }
 };
