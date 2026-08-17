9cff256a010dd25ec484f0adc5edd6d4b2a66595
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-17 12:15:08 +0530

    Time: 3 ms (76.02%) | Memory: 131.3 MB (33.93%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.cpp
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -10,23 +10,10 @@ public:
         int mn = min(v[1], v[2]);
         v[1] -= mn;
         v[2] -= mn;
-        // cout << v[0] << "-" << v[1] << " " << v[2] << endl;
         if(v[2]) {
             v[2]--;
             if(v[2] == 0) return false;
             return v[0] % 2 == 0;
-            // int mn2 = min(v[2], v[0]);
-            // // cout << mn2 << endl;
-            // v[2] -= mn2;
-            // v[0] -= mn2;
-            // // cout << v[0] << " " << v[1] << " " << v[2] << endl;
-            // if(v[0] == 0 && v[1] == 0 && v[2] == 0) return false;
-            // if(v[0]) {
-            //     return false;
-            // }
-            // if(v[2]) {
-            //     return true;
-            // }
         }
         v[0] -= v[0] / 2 * 2;
         if(v[0] == 0 && v[1] == 0 && v[2] == 0) return false;
@@ -46,7 +33,6 @@ public:
         bool a = fun1(v);
         swap(v[1], v[2]);
         bool b = fun1(v);
-        cout << a << " " << b;
         return a || b;
     }
 };
