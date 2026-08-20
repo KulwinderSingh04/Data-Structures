5f4831879438d77e6c9a7e54e83e11d69c263e51
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-20 11:44:04 +0530

    Time: 540 ms (18.14%) | Memory: 125.4 MB (48.60%) - LeetSync

--- 4386-elevator-requests-iii/elevator-requests-iii.cpp
+++ 4386-elevator-requests-iii/elevator-requests-iii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     typedef long long ll;
-    ll fun(ll mask, ll last, int start, vector<vector<int>>& req, vector<vector<ll>>& dp) {
-        ll prevMask = mask ^ (1 << last);
+    ll fun(int mask, int last, int start, vector<vector<int>>& req, vector<vector<ll>>& dp) {
+        int prevMask = mask ^ (1 << last);
+        int n = req.size();
         if(prevMask == 0) {
-            return dp[mask][last] = max(1LL * abs(start - req[last][1]), 1LL * req[last][0]);
+            return max(abs(start - req[last][1]), req[last][0]);
         }
         if(dp[mask][last] != -1) return dp[mask][last];
         ll ans = 1e18;
-        for(int i = 0; i < req.size(); i++) {
+        for(int i = 0; i < n; i++) {
             if((prevMask & (1 << i)) == 0) continue;
-            ll prevTime = fun(prevMask, i, start, req, dp);
-            ll curr = max(prevTime + abs(req[last][1] - req[i][1]), 1LL * req[last][0]);
-            ans = min(ans, curr);
+            ans = min(ans, max(abs(req[i][1] - req[last][1])  + fun(prevMask, i, start, req, dp), 1LL * req[last][0]));
         }
         return dp[mask][last] = ans;
     }
     long long elevatorRequests(int n, int start, vector<vector<int>>& req) {
-        int m = req.size();
-        ll full = (1 << m) - 1;
-        vector<vector<ll>> dp((1 << m), vector<ll> (m, -1));
-        ll res = 1e18;
-        for(int i = 0; i < req.size(); i++) {
-            res = min(res, fun(full, i, start, req, dp));
+        ll ans = 1e18;
+        int sz = req.size();
+        int mask = (1 << sz) - 1;
+        vector<vector<ll>> dp(mask + 1, vector<ll> (sz, -1));
+        for(int i = 0; i < sz; i++) {
+            ans = min(ans, fun(mask, i, start, req, dp));
         }
-        return res;
+        return ans;
     }
 };
