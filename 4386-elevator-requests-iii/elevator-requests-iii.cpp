e41b0832028b2cc54c0aff835c74ca4ee9089a52
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-20 12:03:10 +0530

    Time: 442 ms (28.68%) | Memory: 125.5 MB (48.60%) - LeetSync

--- 4386-elevator-requests-iii/elevator-requests-iii.cpp
+++ 4386-elevator-requests-iii/elevator-requests-iii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     typedef long long ll;
-    ll fun(int mask, int last, int start, vector<vector<int>>& req, vector<vector<ll>>& dp) {
-        int prevMask = mask ^ (1 << last);
-        int n = req.size();
-        if(prevMask == 0) {
-            return max(abs(start - req[last][1]), req[last][0]);
-        }
-        if(dp[mask][last] != -1) return dp[mask][last];
-        ll ans = 1e18;
-        for(int i = 0; i < n; i++) {
-            if((prevMask & (1 << i)) == 0) continue;
-            ans = min(ans, max(abs(req[i][1] - req[last][1])  + fun(prevMask, i, start, req, dp), 1LL * req[last][0]));
-        }
-        return dp[mask][last] = ans;
-    }
     long long elevatorRequests(int n, int start, vector<vector<int>>& req) {
-        ll ans = 1e18;
         int sz = req.size();
-        int mask = (1 << sz) - 1;
-        vector<vector<ll>> dp(mask + 1, vector<ll> (sz, -1));
+        vector<vector<ll>> dp((1 << sz), vector<ll> (sz, 1e18));
+        for(int l = 0; l < sz; l++) {
+            dp[1 << l][l] = max(abs(start - req[l][1]), req[l][0]);
+        }
+        for(int mask = 1; mask < (1 << sz) - 1; mask++) {
+            for(int last = 0; last < sz; last++) { 
+                if((mask & (1 << last))) continue;
+                int newMask = mask | (1 << last);
+                for(int i = 0; i < sz; i++) {
+                    if((mask & (1 << i)) == 0) continue;
+                    dp[newMask][last] = min(dp[newMask][last], max(abs(req[i][1] - req[last][1]) + dp[mask][i], 1LL * req[last][0]));
+                }
+            }
+        }
+        ll ans = 1e18;
         for(int i = 0; i < sz; i++) {
-            ans = min(ans, fun(mask, i, start, req, dp));
+            ans = min(ans, dp[(1 << sz) - 1][i]);
         }
         return ans;
     }
