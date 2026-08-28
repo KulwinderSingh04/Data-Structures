f8417cd5257bd9e5ee41932e39861cf788de091d
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-08-28 11:41:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (89.88%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    bool f(int i, string &t, vector<int> &cnt, string &r, bool g) {
-        if (i == t.size()) return g;
-        for (int c = 0; c < 26; c++) {
-            if (!cnt[c]) continue;
-            char ch = 'a' + c;
-            if (!g && ch < t[i]) continue;
-            bool ng = g || ch > t[i];
-            cnt[c]--;
+    bool fun(int i, bool g, vector<int>& cnt, string& r, string& target) {
+        if(i == target.size()) return g;
+        for(int ch = 'a'; ch <= 'z'; ch++) {
+            if(cnt[ch - 'a'] == 0) continue;
+            if(g == 0 && ch < target[i]) continue;
+            int ng = g;
+            if(target[i] < ch) ng = true;
+            cnt[ch - 'a']--;
             r.push_back(ch);
-            if (f(i + 1, t, cnt, r, ng)) return true;
+            if(fun(i + 1, ng, cnt, r, target)) return true;
             r.pop_back();
-            cnt[c]++;
+            cnt[ch - 'a']++;
         }
         return false;
     }
-
-    string lexGreaterPermutation(string s, string t) {
+    string lexGreaterPermutation(string s, string target) {
+        int n = s.size();
+        string r = "";
         vector<int> cnt(26);
-        for (char c : s) cnt[c - 'a']++;
-        string r;
-        if (f(0, t, cnt, r, false)) return r;
+        for(auto x : s) cnt[x - 'a']++;
+        if(fun(0, 0, cnt, r, target)) return r;
         return "";
     }
 };
