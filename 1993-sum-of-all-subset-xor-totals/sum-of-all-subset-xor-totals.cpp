98b7776ec91db24ffa96247e2ada36b2a9c97efa
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-09-07 15:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (34.64%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    string convert(int a) {
-        string s = "";
-        while(a != 0) {
-            s += a % 2 + 48;
-            a /= 2;
-        }
-        
-        reverse(s.begin(), s.end());
-        return s;
-    }
-    int revCon(string s) {
+    int subsetXORSum(vector<int>& nums) {
+        int o = 0;
+        int n = nums.size();
         int ans = 0;
-        int n = s.size();
-        for(int i = n - 1; i >= 0; i--) {
-            ans += (s[i] - 48) * pow(2, n - 1 - i);
+        for(auto x : nums) o |= x;
+        for(int i = 0; i < 32; i++) {
+            if(o & (1 << i)) {
+                ans += (1 << (n - 1)) * (1 << i);
+            }
         }
         return ans;
-    }
-    int subsetXORSum(vector<int>& nums) {
-        int n = nums.size();
-        int a = 0;
-        for(int i = 0; i < n; i++) a |= nums[i];
-        string s = convert(a);
-        for(int i = 0; i < n -1; i++) s += '0';
-        return revCon(s);
     }
 };
