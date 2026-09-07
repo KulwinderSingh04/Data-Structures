c0c1198bddc41b9aa224a5062a545c7b92604c9b
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-09-07 16:03:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (17.69%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -5,11 +5,6 @@ public:
         int n = nums.size();
         int ans = 0;
         for(auto x : nums) o |= x;
-        for(int i = 0; i < 32; i++) {
-            if(o & (1 << i)) {
-                ans += (1 << (n - 1)) * (1 << i);
-            }
-        }
-        return ans;
+        return o * (1 << (n - 1));
     }
 };
