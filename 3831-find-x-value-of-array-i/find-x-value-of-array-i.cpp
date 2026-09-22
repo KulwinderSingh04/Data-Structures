35fa7ef93bd536a0e00dae193a3685b812df6d13
Kulwinder singh <kulwindersingh157679@gmail.com>
2026-09-22 14:21:44 +0530

    Time: 183 ms (24.11%) | Memory: 173 MB (28.57%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    long long fun(int i, int pr, int rem, int k, vector<int>& nums, vector<vector<long long>>& dp) {
-        int n = nums.size();
-        long long ans = 0;
-        if(pr == rem) ans++;
-        if(i == n) return ans;
-        if(dp[i][pr] != -1) return dp[i][pr];
-        if(pr == k) {
-            ans += fun(i + 1, nums[i] % k, rem, k, nums, dp);
-            ans += fun(i + 1, pr, rem, k, nums, dp);
-        } else {
-            ans += fun(i + 1, 1LL * pr * nums[i] % k, rem, k, nums, dp);
-        }
-        return dp[i][pr] = ans;
-    }
     vector<long long> resultArray(vector<int>& nums, int k) {
-        vector<long long> ans;
         int n = nums.size();
-        for(int i = 0; i < k; i++) {
-            vector<vector<long long>> dp(n, vector<long long> (k + 1, -1));
-            ans.push_back(fun(0, k, i, k, nums, dp));
+        vector<vector<long long>> dp(n, vector<long long> (k));
+        
+        for(int i = 0; i < n; i++) {
+            dp[i][nums[i] % k]++;
+            for(int rem = 0; rem < k; rem++) {
+                if(i != 0) {
+                    dp[i][1LL * nums[i] * rem % k] += dp[i - 1][rem];
+                }
+                
+            }
+        }
+        vector<long long> ans(k);
+        for(int rem = 0; rem < k; rem++) {
+            for(int i = 0; i < n; i++) {
+                ans[rem] += dp[i][rem];
+            }
         }
         return ans;
     }
